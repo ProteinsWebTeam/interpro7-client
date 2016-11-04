6a6f41713de274a236bfdaa586cb019a9f40601c
Aurélien Luciani <luciani@ebi.ac.uk>
2016-11-04 12:53:28 +0000

    fix global styling that made content overflow

--- src/Root.js
+++ src/Root.js
@@ -4,13 +4,14 @@ import React, {PropTypes as T} from 'react';
 // Global stylesheets loaded here
 import 'styles/foundation';
 import 'styles/ebi-global.css';
+import 'styles/global.css';
 import 'styles/interpro-new.css';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import SideMenu from 'components/SideMenu';
 import Breadcrumb from 'components/Breadcrumb';
-import Loading from 'Loading';
+import Loading from 'components/Loading';
 
 const Root = (
   {children, location}/*: {children: Node, location: {pathname: string}}*/
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, {PropTypes as T, Component, cloneElement} from 'react';
+import {connect} from 'react-redux';
 
-const Loading = () => (
-  <div style={{backgroundColor: 'orange'}}>
-    Loading...
-  </div>
-);
+import classnames from 'classnames/bind';
 
-export default Loading;
+import style from './style.css';
+
+const s = classnames.bind(style);
+
+const HALF_VISIBLE = 0.5;
+const VISIBLE = 1;
+
+class Loading extends Component {
+  static propTypes = {
+    data: T.shape({
+      keyUrl: T.string,
+      loading: T.bool.isRequired,
+      data: T.object,
+      error: T.any,
+    }),
+    children: T.element,
+  };
+
+  state = {};
+
+  componentWillMount() {
+    const {data, children} = this.props;
+    if (this.shouldSetState({data, children})) this.setState({data, children});
+  }
+
+  componentWillReceiveProps({data, children}) {
+    if (this.shouldSetState({data, children})) this.setState({data, children});
+  }
+
+  shouldSetState({data, children}) {
+    return (
+      data && children && !data.loading &&
+      (!children.dataUrlMatch || children.dataUrlMatch.test(data.urlKey))
+    );
+  }
+
+  render() {
+    const {data: {loading}, children: _, ...otherProps} = this.props;
+    const {data, children} = this.state;
+
+    return (
+      <div id="content" role="main" className={style.wrapper}>
+        <div className={s('overlay', {loading})}>Loading</div>
+        <div className={s('container', {loading})}>
+          {data && children ?
+            cloneElement(children, {...otherProps, ...data}) :
+            <div>Loading...</div>
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(({data}) => ({data}))(Loading);
