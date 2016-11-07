b56c9c7bfe5401ab1ddbb0ab4238b70c6b729f91
Aurélien Luciani <luciani@ebi.ac.uk>
2016-11-07 10:33:43 +0000

    changes to Sequence component

--- src/components/Protein/Sequence/index.js
+++ src/components/Protein/Sequence/index.js
@@ -5,18 +5,15 @@ import pStyles from './style.css';
 
 const f = foundationPartial(pStyles);
 
-const defaultWordSize = 10;
-
-const _formatSequence = (sequence, wordSize) =>
-  sequence.replace(
-    new RegExp(`(.{${wordSize}})`, 'g'),
-    '$1 '
-  ).split(' ').map((e, i) => (
-    <span key={i} start={wordSize * i} className={f('sequence_word')}>{e} </span>
-  ));
+const _formatSequence = sequence => (
+  // Split every 10 character
+  sequence.match(/.{1,10}/g).map((e, i) => (
+    <span key={i} className={f('sequence_word')}>{e} </span>
+  ))
+);
 
 
-const Sequence = ({wordSize = defaultWordSize, children}) => (
+const Sequence = ({children}) => (
   <section id="sequence">
     <div className={f('row')}>
       <div className={f('large-12', 'columns')}><h4>Sequence</h4></div>
@@ -24,7 +21,7 @@ const Sequence = ({wordSize = defaultWordSize, children}) => (
     <div className={f('row')}>
       <div className={f('large-12', 'columns')}>
         <div className={f('sequence')}>
-          {_formatSequence(children, wordSize)}
+          {children && _formatSequence(children)}
         </div>
       </div>
     </div>
