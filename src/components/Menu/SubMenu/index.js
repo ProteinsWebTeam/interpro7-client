e76e9aa8bf801c05e07086ddb9dfb96bb6c3c720
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2016-11-10 17:30:53 +0000

    submenu in the side menu

---
+++ src/components/Menu/SubMenu/index.js
@@ -0,0 +1,58 @@
+// @flow
+import React, {PropTypes as T, Component} from 'react';
+
+import MenuItem from 'components/Menu/MenuItem';
+
+import {foundationPartial} from 'styles/foundation';
+import menuItemStyle from 'components/Menu/MenuItem/style.css';
+import style from './style.css';
+
+const f = foundationPartial(menuItemStyle, style);
+
+class SubMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isActive: false};
+  }
+  handleClick = () => {
+    this.setState({isActive: !this.state.isActive});
+  }
+  render() {
+    const {pathname, options, className, children} = this.props;
+    /*: {pathname: string, className?: string, options: array, children?: any} */
+    return (
+      <li className={f('is-drilldown-submenu-parent')}>
+        <a className={f('menu-item')} onClick={this.handleClick}>
+          {children}
+        </a>
+        <ul
+          className={`${f('is-drilldown-submenu', 'submenu', {
+            'is-active': this.state.isActive,
+          })} ${className}`}
+        >
+          <li className={f('js-drilldown-back')}>
+            <a className={f('menu-item')} onClick={this.handleClick}>
+              Back InterPro menu
+            </a>
+          </li>
+          {options.map(({to, name}) => (
+            <li key={to}>
+              <MenuItem to={to} active={pathname === to}>
+                {name}
+              </MenuItem>
+            </li>
+          ))}
+        </ul>
+      </li>
+    );
+  }
+
+}
+SubMenu.propTypes = {
+  pathname: T.string.isRequired,
+  className: T.string,
+  options: T.array,
+  children: T.any,
+};
+
+export default SubMenu;
