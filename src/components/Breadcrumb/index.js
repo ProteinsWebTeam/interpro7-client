4e3468bdde4f5b1f0e9f4ebc4fb74098d2ca47e4
pesseat <Rupture26>
2017-06-30 16:59:46 +0100

    replace 100vw by 100% for  bug horizontal scroll on Windows

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import T from 'prop-types';
-import {connect} from 'react-redux';
-import {createSelector} from 'reselect';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import Link from 'components/generic/Link';
 
 import cfg from 'config';
-import {sticky as supportsSticky} from 'utils/support';
+import { sticky as supportsSticky } from 'utils/support';
 
-import {foundationPartial} from 'styles/foundation';
+import { foundationPartial } from 'styles/foundation';
 import ebiStyles from 'styles/ebi-global.css';
 import interproStyles from 'styles/interpro-new.css';
 import helperClasses from 'styles/helper-classes.css';
@@ -19,7 +19,7 @@ const f = foundationPartial(ebiStyles, interproStyles, helperClasses, style);
 const parts = new Set(Object.keys(cfg.pages));
 
 const fullPathToPartialPaths = fullPath => {
-  const partialPaths = [{url: '/', name: 'Home'}];
+  const partialPaths = [{ url: '/', name: 'Home' }];
   for (const name of fullPath.split('/').filter(x => x)) {
     partialPaths.push({
       url: `${partialPaths[partialPaths.length - 1].url}${name}/`,
@@ -41,20 +41,25 @@ const getPaths = fullPath => {
   }
   pathGroups.push(working);
   const [[home], view, ...focus] = pathGroups;
-  return {home, view, focus};
+  return { home, view, focus };
 };
 
 const positions = new WeakMap();
 
-const mapPathArrayToLink = paths => paths.map(({url, name}, i) => (
-  null && <Link key={url || i} to={url || '#'}>{name}</Link>
-));
+const mapPathArrayToLink = paths =>
+  paths.map(
+    ({ url, name }, i) =>
+      null &&
+      <Link key={url || i} to={url || '#'}>
+        {name}
+      </Link>,
+  );
 
-const formatEndpoints = paths => ([
-  {paths: [paths.home, {name: '…'}], type: 'home'},
-  {paths: [...(paths.view || {}), {name: '…'}], type: 'view'},
-  ...paths.focus.map(f => ({paths: [...f, {name: '…'}], type: 'focus'})),
-]);
+const formatEndpoints = paths => [
+  { paths: [paths.home, { name: '…' }], type: 'home' },
+  { paths: [...(paths.view || {}), { name: '…' }], type: 'view' },
+  ...paths.focus.map(f => ({ paths: [...f, { name: '…' }], type: 'focus' })),
+];
 
 class Breadcrumb extends Component {
   static propTypes = {
@@ -99,22 +104,25 @@ class Breadcrumb extends Component {
 
   handleUpdate = () => {
     if (!this._node || !this._node.animate) return;
-    const nodes = [...this._node.parentElement.getElementsByClassName(f('flip'))];
+    const nodes = [
+      ...this._node.parentElement.getElementsByClassName(f('flip')),
+    ];
     for (const node of nodes) {
       const curr = node.getBoundingClientRect();
       const prev = positions.get(node);
       if (prev) {
         console.log(
-          `translate(${prev.left - curr.left}px, ${prev.top - curr.top}px)`
+          `translate(${prev.left - curr.left}px, ${prev.top - curr.top}px)`,
         );
         node.animate(
           [
-            {transform: (
-              `translate(${prev.left - curr.left}px, ${prev.top - curr.top}px)`
-            )},
-            {transform: 'translate(0, 0)'},
+            {
+              transform: `translate(${prev.left - curr.left}px, ${prev.top -
+                curr.top}px)`,
+            },
+            { transform: 'translate(0, 0)' },
           ],
-          {duration: 500, easing: 'ease-out'}
+          { duration: 500, easing: 'ease-out' },
         );
       }
       positions.set(node, curr);
@@ -122,18 +130,18 @@ class Breadcrumb extends Component {
   };
 
   expand = () => {
-    this.setState({expanded: true});
+    this.setState({ expanded: true });
   };
 
   reduce = () => {
-    this.setState({expanded: false});
+    this.setState({ expanded: false });
   };
 
   render() {
-    const {expanded, paths} = this.state;
+    const { expanded, paths } = this.state;
     const pathname = this.props;
     const endpoints = formatEndpoints(paths, expanded, this.props);
-    const {stickyMenuOffset: offset, stuck} = this.props;
+    const { stickyMenuOffset: offset, stuck } = this.props;
     const MAGIC = 88;
     return (
       <div
@@ -142,24 +150,26 @@ class Breadcrumb extends Component {
           flexShrink: 0,
           marginTop: supportsSticky && stuck ? `${offset + MAGIC}px` : '0',
         }}
-        ref={node => this._node = node}
+        ref={node => (this._node = node)}
       >
-        <div className={f('columns', 'large-12')} style={{width: '100vw'}}>
+        <div className={f('columns', 'large-12')} style={{ width: '100%' }}>
           <nav
-            style={{display: pathname === '/' ? 'none' : ''}}
-            className={f('breadcrumbs', {expanded, standard: !expanded})}
-            onFocus={this.expand} onBlur={this.reduce}
-            onMouseEnter={this.expand} onMouseLeave={this.reduce}
+            style={{ display: pathname === '/' ? 'none' : '' }}
+            className={f('breadcrumbs', { expanded, standard: !expanded })}
+            onFocus={this.expand}
+            onBlur={this.reduce}
+            onMouseEnter={this.expand}
+            onMouseLeave={this.reduce}
           >
             <span className={f('groups')}>
-              {endpoints.map((endpoint, i) => (
-                <span key={i} className={f({group: i <= 1, focus: i > 1})}>
+              {endpoints.map((endpoint, i) =>
+                <span key={i} className={f({ group: i <= 1, focus: i > 1 })}>
                   {mapPathArrayToLink(endpoint.paths)}
-                </span>
-              ))}
+                </span>,
+              )}
             </span>
             <span className={f('hint')}>
-              <Link newTo={{description: {other: 'help'}}} title="help">
+              <Link newTo={{ description: { other: 'help' } }} title="help">
                 <div>main view</div>
                 <div>focus</div>
               </Link>
@@ -174,7 +184,7 @@ class Breadcrumb extends Component {
 const mapStateToProps = createSelector(
   state => state.ui.stuck,
   state => state.location.pathname,
-  (stuck, pathname) => ({stuck, pathname})
+  (stuck, pathname) => ({ stuck, pathname }),
 );
 
 export default connect(mapStateToProps)(Breadcrumb);
