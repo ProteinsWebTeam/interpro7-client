48b81165b9dbf664cefee1753908733b7a91dacf
pesseat <Rupture26>
2017-07-25 17:27:57 +0100

    remove ebiTheme  and ebiPetrolTheme and just use interProTheme

--- src/components/EMBLDropdown/index.js
+++ src/components/EMBLDropdown/index.js
@@ -1,18 +1,20 @@
 // @flow
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import T from 'prop-types';
-import {connect} from 'react-redux';
-import {createSelector} from 'reselect';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
-import {foundationPartial} from 'styles/foundation';
+import { foundationPartial } from 'styles/foundation';
 
 import styles from './style.css';
 import ebiGlobalStyles from 'styles/ebi-global.css';
 import fonts from 'styles/ebi/fonts.css';
-import ebiTheme from 'styles/theme-template.css';
-import ebiPetrolTheme from 'styles/theme-embl-petrol.css';
+import interproTheme from 'styles/theme-interpro.css';
 const styleBundle = foundationPartial(
-  styles, fonts, ebiPetrolTheme, ebiTheme, ebiGlobalStyles
+  styles,
+  fonts,
+  interproTheme,
+  ebiGlobalStyles,
 );
 
 class EMBLDropdown extends PureComponent {
@@ -21,13 +23,13 @@ class EMBLDropdown extends PureComponent {
   };
 
   render() {
-    const {visible} = this.props;
+    const { visible } = this.props;
     return (
       <div
         id="embl-dropdown"
-        className={
-          styleBundle('dropdown-pane', 'bottom', 'embl-dropdown', {visible})
-        }
+        className={styleBundle('dropdown-pane', 'bottom', 'embl-dropdown', {
+          visible,
+        })}
       >
         <p>
           EMBL-EBI in Hinxton is one of six EMBL locations across Europe.<br />
@@ -35,62 +37,87 @@ class EMBLDropdown extends PureComponent {
             href="https://www.ebi.ac.uk/about"
             className={styleBundle('small', 'readmore')}
             rel="noopener"
-          >More about EMBL-EBI</a>
+          >
+            More about EMBL-EBI
+          </a>
         </p>
         <h6>Connect to another EMBL location</h6>
         <div
-          className={
-            styleBundle(
-              'row', 'small-collapse', 'clearfix', 'padding-bottom-large'
-            )
-          }
+          className={styleBundle(
+            'row',
+            'small-collapse',
+            'clearfix',
+            'padding-bottom-large',
+          )}
         >
           <div
-            className={
-              styleBundle('columns', 'small-5', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-5',
+              'padding-bottom-medium',
+            )}
           >
-            <a href="https://www.embl.de/" rel="noopener">Heidelberg</a>
+            <a href="https://www.embl.de/" rel="noopener">
+              Heidelberg
+            </a>
             <div className={styleBundle('small')}>Main Laboratory</div>
           </div>
           <div
-            className={
-              styleBundle('columns', 'small-7', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-7',
+              'padding-bottom-medium',
+            )}
           >
-            <a href="http://www.embl-barcelona.es/" rel="noopener">Barcelona</a>
+            <a href="http://www.embl-barcelona.es/" rel="noopener">
+              Barcelona
+            </a>
             <div className={styleBundle('small')}>
               Tissue biology and disease modelling
             </div>
           </div>
           <div
-            className={
-              styleBundle('columns', 'small-5', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-5',
+              'padding-bottom-medium',
+            )}
           >
-            <a href="https://www.embl.fr/" rel="noopener">Grenoble</a>
+            <a href="https://www.embl.fr/" rel="noopener">
+              Grenoble
+            </a>
             <div className={styleBundle('small')}>Structural Biology</div>
           </div>
           <div
-            className={
-              styleBundle('columns', 'small-7', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-7',
+              'padding-bottom-medium',
+            )}
           >
-            <a href="https://www.embl-hamburg.de/" rel="noopener">Hamburg</a>
+            <a href="https://www.embl-hamburg.de/" rel="noopener">
+              Hamburg
+            </a>
             <div className={styleBundle('small')}>Structural Biology</div>
           </div>
           <div
-            className={
-              styleBundle('columns', 'small-5', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-5',
+              'padding-bottom-medium',
+            )}
           >
-            <a href="https://www.embl.it/" rel="noopener">Monterotondo</a>
+            <a href="https://www.embl.it/" rel="noopener">
+              Monterotondo
+            </a>
             <div className={styleBundle('small')}>Mouse Biology</div>
           </div>
           <div
-            className={
-              styleBundle('columns', 'small-7', 'padding-bottom-medium')
-            }
+            className={styleBundle(
+              'columns',
+              'small-7',
+              'padding-bottom-medium',
+            )}
           >
             <a
               href="http://embl.org/"
@@ -108,7 +135,7 @@ class EMBLDropdown extends PureComponent {
 
 const mapStateToProps = createSelector(
   state => state.ui.emblMapNav,
-  visible => ({visible}),
+  visible => ({ visible }),
 );
 
 export default connect(mapStateToProps)(EMBLDropdown);
