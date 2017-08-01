2abc4401bee85e810bc49f3f63c997fab617ef0d
pesseat <Rupture26>
2017-08-01 15:50:39 +0100

    Add a class to the empty div

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,5 +1,5 @@
 /* eslint react/jsx-pascal-case: 0 */
-import React, {Children} from 'react';
+import React, { Children } from 'react';
 import T from 'prop-types';
 
 import _Header from './Header';
@@ -12,10 +12,10 @@ import _Column from './Column';
 import _Row from './Row';
 import _Footer from './Footer';
 
-import classname from 'classnames/bind';
-import style from './style.css';
+import { foundationPartial } from 'styles/foundation';
+import styles from './style.css';
 
-const s = classname.bind(style);
+const f = foundationPartial(styles);
 
 // const getData = (data, staleData) => {
 //   if (!data.loading) return data;
@@ -23,8 +23,16 @@ const s = classname.bind(style);
 //   return {payload: {results: [], count: 0}};
 // };
 
-const Table = (
-  {dataTable, isStale, actualSize, query, pathname, title, notFound, children}
+const Table = ({
+  dataTable,
+  isStale,
+  actualSize,
+  query,
+  pathname,
+  title,
+  notFound,
+  children,
+}) =>
   /*: {
    dataTable: Array<Object>,
    isStale: ?boolean,
@@ -33,45 +41,43 @@ const Table = (
    pathname: string,
    children?: any
    } */
-) => {
-  const _query = query || {};
-  const _children = Children.toArray(children);
-  // Extract prop information out of every Column element's props
-  const columns = _children.filter(child => child.type === _Column)
-    .map(child => child.props);
-  const search = _children.find(child => child.type === _SearchBox);
-  const pageSize = _children.find(child => child.type === _PageSizeSelector);
-  const exporter = _children.find(child => child.type === _Exporter);
+  {
+    const _query = query || {};
+    const _children = Children.toArray(children);
+    // Extract prop information out of every Column element's props
+    const columns = _children
+      .filter(child => child.type === _Column)
+      .map(child => child.props);
+    const search = _children.find(child => child.type === _SearchBox);
+    const pageSize = _children.find(child => child.type === _PageSizeSelector);
+    const exporter = _children.find(child => child.type === _Exporter);
 
-  return (
-    <div>
-      {title && <h4>{title}</h4>}
-      {exporter}
-      {
-        pageSize &&
-        <_PageSizeSelector
-          search={_query}
-          pathname={pathname}
-        />
-      }
-      {search}
-      <table className={s('table', {isStale})}>
-        <_Header columns={columns} />
-        <_Body
-          rows={dataTable || []}
-          columns={columns}
-        />
-      </table>
-      <_Footer
-        data={dataTable}
-        actualSize={actualSize}
-        pagination={_query}
-        pathname={pathname}
-        notFound={notFound}
-      />
-    </div>
-  );
-};
+    return (
+      <div className={f('row')}>
+        <div className={f('columns', 'table-view')}>
+          {title &&
+            <h4>
+              {title}
+            </h4>}
+          {exporter}
+          {pageSize &&
+            <_PageSizeSelector search={_query} pathname={pathname} />}
+          {search}
+          <table className={f('table', { isStale })}>
+            <_Header columns={columns} />
+            <_Body rows={dataTable || []} columns={columns} />
+          </table>
+          <_Footer
+            data={dataTable}
+            actualSize={actualSize}
+            pagination={_query}
+            pathname={pathname}
+            notFound={notFound}
+          />
+        </div>
+      </div>
+    );
+  };
 Table.propTypes = {
   dataTable: T.array,
   isStale: T.bool,
