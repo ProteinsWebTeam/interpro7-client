dd4f538d34dc15edf7746d3e61d4e2a51550ce4f
pesseat <Rupture26>
2017-08-03 17:07:31 +0100

    add basic grid and import foundation style

--- src/staticPages/About/index.js
+++ src/staticPages/About/index.js
@@ -1,19 +1,25 @@
 // @flow
 import React from 'react';
-
+import styles from './styles.css';
+import { foundationPartial } from 'styles/foundation';
+const f = foundationPartial(styles);
 import info from './info';
 
 export default () =>
-  <section>
-    <h4>About this website</h4>
-    <h5>Developer information</h5>
-    <p>
-      This website has been built at{' '}
-      <code>{String(new Date(info.build.time))}</code>
-    </p>
-    <p>
-      It has been built from the repository at <code>{info.git.remote}</code>,
-      branch <code>{info.git.branch}</code>, hash{' '}
-      <code title={`tag: ${info.git.tag}`}>{info.git.hash}</code>
-    </p>
-  </section>;
+  <div className={f('row')}>
+    <div className={f('columns', 'large-12')}>
+      <section>
+        <h4>About this website</h4>
+        <h5>Developer information</h5>
+        <p>
+          This website has been built at{' '}
+          <code>{String(new Date(info.build.time))}</code>
+        </p>
+        <p>
+          It has been built from the repository at{' '}
+          <code>{info.git.remote}</code>, branch <code>{info.git.branch}</code>,
+          hash <code title={`tag: ${info.git.tag}`}>{info.git.hash}</code>
+        </p>
+      </section>
+    </div>
+  </div>;
