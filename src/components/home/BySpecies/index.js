9650f12dd8fd7b6e933f041c40dec4c27d9b3b92
pesseat <Rupture26>
2017-08-24 18:17:22 +0100

    add local style and include a div with classname Species-list

--- src/components/home/BySpecies/index.js
+++ src/components/home/BySpecies/index.js
@@ -16,47 +16,56 @@ import ipro from 'styles/interpro-new.css';
 import ebiGlobalStyles from 'ebi-framework/css/ebi-global.scss';
 import fonts from 'EBI-Icon-fonts/fonts.css';
 import theme from 'styles/theme-interpro.css';
-import local from '../ByMemberDatabase/styles.css';
+import local from './styles.css';
 
 const f = foundationPartial(ebiGlobalStyles, fonts, ipro, theme, local);
 
 const BySpecies = ({ data: { payload } }) =>
-  <AnimatedEntry className={f('row')} element="div">
-    {// TODO: Include number of entries
-    speciesFeat.map(e =>
-      <div
-        className={f('column', 'small-3', 'medium-2', 'large-4', 'text-center')}
-        key={e.tax_id}
-      >
-        <Link
-          newTo={location => ({
-            description: {
-              mainType: 'protein',
-              mainDB: 'uniprot',
-            },
-            search: {
-              ...location.search,
-              tax_id: e.tax_id,
-            },
-            hash: location.hash,
-          })}
+  <div className={f('species-list')}>
+    <AnimatedEntry className={f('row')} element="div">
+      {// TODO: Include number of entries
+      speciesFeat.map(e =>
+        <div
+          className={f(
+            'column',
+            'small-3',
+            'medium-2',
+            'large-4',
+            'text-center',
+          )}
+          key={e.tax_id}
         >
-          <span
-            style={{ color: e.color }}
-            className={f('small', 'icon', 'icon-species')}
-            data-icon={e.icon}
-            data-tooltip
-          />
-          <h6>
-            {e.title}
-          </h6>
-          <p>
-            {payload && payload[e.tax_id] ? payload[e.tax_id] : '...'} proteins
-          </p>
-        </Link>
-      </div>,
-    )}
-  </AnimatedEntry>;
+          <Link
+            newTo={location => ({
+              description: {
+                mainType: 'protein',
+                mainDB: 'uniprot',
+              },
+              search: {
+                ...location.search,
+                tax_id: e.tax_id,
+              },
+              hash: location.hash,
+            })}
+          >
+            <span
+              style={{ color: e.color }}
+              className={f('small', 'icon', 'icon-species')}
+              data-icon={e.icon}
+              data-tooltip
+            />
+            <h6>
+              {e.title}
+            </h6>
+            <p>
+              {payload && payload[e.tax_id] ? payload[e.tax_id] : '...'}{' '}
+              proteins
+            </p>
+          </Link>
+        </div>,
+      )}
+    </AnimatedEntry>;
+  </div>;
 
 BySpecies.propTypes = {
   data: T.shape({
