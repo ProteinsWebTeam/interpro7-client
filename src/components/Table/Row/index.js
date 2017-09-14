aae08dbad5e38f763f6bf935b281231dc970b85e
pesseat <Rupture26>
2017-09-14 10:57:35 +0100

    remove animation on table that made the table row border appear in a strange order

--- src/components/Table/Row/index.js
+++ src/components/Table/Row/index.js
@@ -10,19 +10,19 @@ class Row extends Component {
     row: T.object.isRequired,
     columns: T.array.isRequired,
   };
-
-  componentDidMount() {
-    if (!this._node.animate) return;
-    // onenter for this node
-    // this._node.animate(
-    //   [{transform: 'translateX(100%)'}, {transform: 'translateX(0)'}],
-    //   {duration: 500, delay: Math.random() * 100, fill: 'both'},
-    // );
-    this._node.animate(
-      { opacity: [0, 1] },
-      { duration: 1000, easing: 'ease-in-out' }
-    );
-  }
+  // TODO review / fix
+  // componentDidMount() {
+  //   if (!this._node.animate) return;
+  //   // onenter for this node
+  //   // this._node.animate(
+  //   //   [{transform: 'translateX(100%)'}, {transform: 'translateX(0)'}],
+  //   //   {duration: 500, delay: Math.random() * 100, fill: 'both'},
+  //   // );
+  //   this._node.animate(
+  //     { opacity: [0, 1] },
+  //     { duration: 1000, easing: 'ease-in-out' }
+  //   );
+  // }
 
   render() {
     const { row, columns } = this.props;
