86ba62d18d949697dc4f55bb9a94940d6d6ffb5e
pesseat <Rupture26>
2017-09-29 14:14:44 +0100

    Create some conditions to change IPR ID background color ( in title) depending on entry type, change the way style is imported to be more consistent, also replace the type attribute in interpro-type to make the type icon work on site (active, binding, etc) entry pages

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -7,8 +7,12 @@ import Link from 'components/generic/Link';
 
 import loadWebComponent from 'utils/loadWebComponent';
 
+import { foundationPartial } from 'styles/foundation';
+
 import ipro from 'styles/interpro-new.css';
-import style from './style.css';
+import styles from './style.css';
+
+const f = foundationPartial(ipro, styles);
 
 /*:: type Props = {
   metadata: {
@@ -43,17 +47,40 @@ export default class Title extends PureComponent /*:: <Props> */ {
     const { metadata, mainType } = this.props;
     const isEntry = mainType === 'entry';
     return (
-      <div className={style.title}>
-        {isEntry && <interpro-type type={metadata.type} size="4em" />}
+      <div className={f('title')}>
+        {isEntry && (
+          <interpro-type type={metadata.type.replace('_', ' ')} size="4em" />
+        )}
         <Helmet>
           <title>{metadata.accession.toString()}</title>
         </Helmet>
         <h3>
-          {metadata.name.name} <small>({metadata.accession})</small>
+          {metadata.name.name}
+          <small
+            className={f(
+              metadata.type === 'Domain'
+                ? 'title-id-domain'
+                : null || metadata.type === 'Family'
+                  ? 'title-id-family'
+                  : null || metadata.type === 'Repeat'
+                    ? 'title-id-repeat'
+                    : null || metadata.type === 'unknow'
+                      ? 'title-id-unknown'
+                      : null ||
+                        metadata.type === 'Conserved_site' ||
+                        metadata.type === 'Binding_site' ||
+                        metadata.type === 'Active_site' ||
+                        metadata.type === 'PTM'
+                        ? 'title-id-site'
+                        : null,
+            )}
+          >
+            {metadata.accession}
+          </small>
         </h3>
         {isEntry &&
           metadata.source_database.toLowerCase() !== 'interpro' && (
-            <div className={ipro['md-hlight']}>
+            <div className={f('md-hlight')}>
               <h5>
                 Member database:&nbsp;
                 <Link
@@ -72,9 +99,7 @@ export default class Title extends PureComponent /*:: <Props> */ {
         {metadata.name.short && (
           <p>
             Short name:&nbsp;
-            <i className="small" style={{ color: '#41647d' }}>
-              {metadata.name.short}
-            </i>
+            <i className={f('shortname')}>{metadata.name.short}</i>
           </p>
         )}
       </div>
