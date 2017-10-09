073da02302e5c130f8bea12bb85e42f0736e76cf
pesseat <Rupture26>
2017-10-09 14:48:41 +0100

    Some formating for the cross reference section

--- src/components/Entry/CrossReferences/index.js
+++ src/components/Entry/CrossReferences/index.js
@@ -6,9 +6,10 @@ import Link from 'components/generic/Link';
 
 import { foundationPartial } from 'styles/foundation';
 
-import local from './styles.css';
+import local from './style.css';
+import ebiStyles from 'ebi-framework/css/ebi-global.scss';
 
-const f = foundationPartial(local);
+const f = foundationPartial(local, ebiStyles);
 
 const ReferenceItem = ({ url, accession }) => (
   <li>
@@ -21,9 +22,9 @@ ReferenceItem.propTypes = {
 };
 
 const ReferenceSection = ({ accessions, name, description }) => (
-  <li className={f('xref-section')}>
+  <li className={f('xref-section', 'small')}>
     <h5>{name}</h5>
-    <div>{description}</div>
+    <span>{description}</span>
     <ul>
       {accessions.map(({ accession, url }) => (
         <ReferenceItem key={accession} accession={accession} url={url} />
@@ -44,12 +45,16 @@ export default class CrossReferences extends PureComponent {
 
   render() {
     const databases = Object.entries(this.props.xRefs).sort(
-      ([a], [b]) => a.rank - b.rank
+      ([a], [b]) => a.rank - b.rank,
     );
     return (
       <div className={f('row')}>
         <div className={f('large-12', 'columns')}>
-          <AnimatedEntry className={f('list')} itemDelay={100} duration={500}>
+          <AnimatedEntry
+            className={f('list', 'margin-left-none')}
+            itemDelay={100}
+            duration={500}
+          >
             {databases.map(
               ([database, { displayName, description, accessions }]) => (
                 <ReferenceSection
@@ -58,7 +63,7 @@ export default class CrossReferences extends PureComponent {
                   description={description}
                   accessions={accessions}
                 />
-              )
+              ),
             )}
           </AnimatedEntry>
         </div>
