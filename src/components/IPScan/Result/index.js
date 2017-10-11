6e9849b3645266bcddee5d0d47a76b26e98671f7
Aurélien Luciani <luciani@ebi.ac.uk>
2017-10-11 15:13:09 +0100

    dump IPScan matches on screen

--- src/components/IPScan/Result/index.js
+++ src/components/IPScan/Result/index.js
@@ -1,12 +1,48 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import T from 'prop-types';
+import { format } from 'url';
+
+import loadData from 'higherOrder/loadData';
 
 class IPScanResult extends PureComponent {
+  static propTypes = {
+    mainAccession: T.string.isRequired,
+  };
+
   render() {
-    console.log(this.props);
-    return <div>{this.props.matched}</div>;
+    const { mainAccession, data: { payload } } = this.props;
+    return (
+      <div>
+        {mainAccession}
+        {payload && (
+          <ul>
+            {payload[0].matches.map((match, i) => (
+              <pre key={i}>{JSON.stringify(match, null, 2)}</pre>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
   }
 }
 
-export default connect()(IPScanResult);
+const mapStateToUrl = createSelector(
+  state => state.settings.ipScan,
+  state => state.newLocation.description.mainAccession,
+  ({ protocol, hostname, port, root }, mainAccession) =>
+    format({
+      protocol,
+      hostname,
+      port,
+      pathname: `${root}/result/${mainAccession}/json`,
+    }),
+);
+
+const mapStateToProps = createSelector(
+  state => state.newLocation.description.mainAccession,
+  mainAccession => ({ mainAccession }),
+);
+
+export default loadData(mapStateToUrl)(connect(mapStateToProps)(IPScanResult));
