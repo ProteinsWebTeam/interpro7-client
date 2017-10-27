7eddf34b22c114b8b6b653d954155bc8182d1928
pesseat <Rupture26>
2017-10-27 11:22:37 +0100

    Remove table header if no data available

--- src/components/Table/Header/index.js
+++ src/components/Table/Header/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import T from 'prop-types';
 
-const Header = ({ columns } /*: {columns: Array<Object>} */) => (
-  <thead>
-    <tr>
-      {columns
-        .filter(({ displayIf = true }) => displayIf)
-        .map(
-          ({ dataKey, defaultKey, name, headerStyle, children, className }) => (
-            <th
-              key={defaultKey || dataKey}
-              style={headerStyle}
-              className={className}
-            >
-              {children || name || dataKey}
-            </th>
-          ),
-        )}
-    </tr>
-  </thead>
-);
+const Header = ({ columns, notFound } /*: {columns: Array<Object>} */) => {
+  if (notFound) {
+    return null;
+  }
+  return (
+    <thead>
+      <tr>
+        {columns
+          .filter(({ displayIf = true }) => displayIf)
+          .map(
+            ({
+              dataKey,
+              defaultKey,
+              name,
+              headerStyle,
+              children,
+              className,
+            }) => (
+              <th
+                key={defaultKey || dataKey}
+                style={headerStyle}
+                className={className}
+              >
+                {children || name || dataKey}
+              </th>
+            ),
+          )}
+      </tr>
+    </thead>
+  );
+};
 Header.propTypes = {
   columns: T.arrayOf(T.object).isRequired,
 };
