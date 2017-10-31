98b4c348938843f9a094333712ec251eae9cde8f
Aurélien Luciani <luciani@ebi.ac.uk>
2017-10-31 10:35:43 +0000

    added "member" for member DB entries

--- src/components/Header/Title/index.js
+++ src/components/Header/Title/index.js
@@ -87,6 +87,10 @@ class Title extends PureComponent /*:: <Props> */ {
               </div>
               <div className={styles('logo-flex-item', 'logo-text')}>
                 InterPro
+                {mainType === 'entry' &&
+                  mainDB.toLowerCase() !== 'interpro' &&
+                  mainAccession &&
+                  ' - Member'}
               </div>
             </div>
           </Link>
@@ -109,7 +113,7 @@ const mapStateToProps = createSelector(
           return datum.loading;
         }
         return false;
-      })
+      }),
   ),
   state => state.newLocation.description.mainType,
   state => state.newLocation.description.mainDB,
@@ -121,7 +125,7 @@ const mapStateToProps = createSelector(
     mainDB,
     mainAccession,
     stuck,
-  })
+  }),
 );
 
 export default connect(mapStateToProps)(Title);
