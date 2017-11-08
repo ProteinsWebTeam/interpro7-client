6ea67c471e0b6c38027d014b4ffedf93f270aa2b
pesseat <Rupture26>
2017-11-08 12:44:45 +0000

    Inserting a new contributing signature box for testing

--- src/components/Entry/ContributingSignatures/index.js
+++ src/components/Entry/ContributingSignatures/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import T from 'prop-types';
 
 import Link from 'components/generic/Link';
-
+import MemberSymbol from 'components/Entry/MemberSymbol';
 import loadable from 'higherOrder/loadable';
 
 import { foundationPartial } from 'styles/foundation';
 import ipro from 'styles/interpro-new.css';
 import local from './style.css';
-
+// import {MaskSvgIcons} from 'pages/Home'
+const MaskSvgIcons = () => (
+  <svg
+    viewBox="0 0 200 200"
+    style={{
+      position: 'fixed',
+      width: 0,
+      height: 0,
+      top: -1800,
+      left: -1800,
+      /* to hide SVG on the page as display:none is not working */
+    }}
+  >
+    <defs>
+      <clipPath id="cut-off-center">
+        <rect x="33%" y="38%" width="68" height="68" />
+      </clipPath>
+      <clipPath id="cut-off-bottom">
+        <polygon points="0,68 68,0 68,68" />
+      </clipPath>
+    </defs>
+  </svg>
+);
 const f = foundationPartial(ipro, local);
 
 const SchemaOrgData = loadable({
@@ -29,37 +51,70 @@ const schemaProcessData = ({ name, db }) => ({
 
 const ContributingSignatures = ({ contr } /*: {contr: Object} */) => (
   <div className={f('side-panel', 'margin-top-small', 'margin-bottom-large')}>
-    <h5>Contributing signatures</h5>
+    <div className={f('md-icon-list-box', 'margin-bottom-large')}>
+      <h5>Contributing signatures</h5>
+      <ul className={f('md-list')}>
+        {Object.entries(contr).map(([db, accessions]) => (
+          <li key={db}>
+            <MaskSvgIcons />
 
-    <div className={f('table-chevron')}>
-      {Object.entries(contr).map(([db, accessions]) => (
-        <div key={db} className={f('sign-row')}>
-          <span className={f('sign-cell')}>{db}</span>
+            <MemberSymbol type={db} className={f('md-small')} />
+            {accessions.map(accession => (
+              <Link
+                key={accession}
+                newTo={{
+                  description: {
+                    mainType: 'entry',
+                    mainDB: db,
+                    mainAccession: accession,
+                  },
+                }}
+              >
+                <div className={f('md-list-text')}>
+                  <small>
+                    <span style={{ color: '#4b555b' }}>{db}:</span>{' '}
+                    <span>{accession}</span>
+                  </small>
+                </div>
+              </Link>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
 
-          {accessions.map(accession => (
-            <span key={accession} className={f('sign-cell')}>
-              <SchemaOrgData
-                data={{ db, name: accession }}
-                processData={schemaProcessData}
-              />
-              <span className={f('sign-label')}>
-                <Link
-                  className={f('neutral')}
-                  newTo={{
-                    description: {
-                      mainType: 'entry',
-                      mainDB: db,
-                      mainAccession: accession,
-                    },
-                  }}
-                >
-                  {accession}
-                </Link>
+    <div className={f('md-list-box', 'margin-bottom-large')}>
+      <h5>Contributing signatures</h5>
+      <div className={f('table-chevron')}>
+        {Object.entries(contr).map(([db, accessions]) => (
+          <div key={db} className={f('sign-row')}>
+            <span className={f('sign-cell')}>{db}</span>
+
+            {accessions.map(accession => (
+              <span key={accession} className={f('sign-cell')}>
+                <SchemaOrgData
+                  data={{ db, name: accession }}
+                  processData={schemaProcessData}
+                />
+                <span className={f('sign-label')}>
+                  <Link
+                    className={f('neutral')}
+                    newTo={{
+                      description: {
+                        mainType: 'entry',
+                        mainDB: db,
+                        mainAccession: accession,
+                      },
+                    }}
+                  >
+                    {accession}
+                  </Link>
+                </span>
               </span>
-            </span>
-          ))}
-        </div>
-      ))}
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   </div>
 );
