8b1cc1bc6772278af14eeb6517451a5f9b62e92e
Aurélien Luciani <luciani@ebi.ac.uk>
2017-11-23 07:43:39 +0000

    fix mess by prettier

--- src/utils/processDescription/emptyDescription/index.js
+++ src/utils/processDescription/emptyDescription/index.js
@@ -1,49 +1,49 @@
 // @flow
 /*:: import type { Description } from 'utils/processDescription/handlers'; */
 
-export default () =>
-  /*: Description */ Object.freeze({
-    main: Object.seal({
-      key: null,
-    }),
-    entry: Object.seal({
-      integration: null,
-      db: null,
-      accession: null,
-      memberDB: null,
-      memberAccession: null,
-      detail: null,
-    }),
-    protein: Object.seal({
-      db: null,
-      accession: null,
-      detail: null,
-    }),
-    structure: Object.seal({
-      db: null,
-      accession: null,
-      chain: null,
-      detail: null,
-    }),
-    organism: Object.seal({
-      db: null,
-      accession: null,
-      proteomeDB: null,
-      proteomeAccession: null,
-    }),
-    set: Object.seal({
-      db: null,
-      accession: null,
-      detail: null,
-    }),
-    search: Object.seal({
-      type: null,
-      value: null,
-    }),
-    job: Object.seal({
-      type: null,
-      accession: null,
-      detail: null,
-    }),
-    other: Object.seal([]),
-  });
+// prettier-ignore
+export default () /*: Description */ => Object.freeze({
+  main: Object.seal({
+    key: null,
+  }),
+  entry: Object.seal({
+    integration: null,
+    db: null,
+    accession: null,
+    memberDB: null,
+    memberAccession: null,
+    detail: null,
+  }),
+  protein: Object.seal({
+    db: null,
+    accession: null,
+    detail: null,
+  }),
+  structure: Object.seal({
+    db: null,
+    accession: null,
+    chain: null,
+    detail: null,
+  }),
+  organism: Object.seal({
+    db: null,
+    accession: null,
+    proteomeDB: null,
+    proteomeAccession: null,
+  }),
+  set: Object.seal({
+    db: null,
+    accession: null,
+    detail: null,
+  }),
+  search: Object.seal({
+    type: null,
+    value: null,
+  }),
+  job: Object.seal({
+    type: null,
+    accession: null,
+    detail: null,
+  }),
+  other: Object.seal([]),
+});
