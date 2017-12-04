094e0988a7e0f4ead1a8d7f5885e025734e66fb7
pesseat <Rupture26>
2017-12-04 11:32:56 +0000

    Update import tooltip

--- src/components/Entry/CrossReferences/index.js
+++ src/components/Entry/CrossReferences/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import T from 'prop-types';
-import { Tooltip } from 'react-tippy';
+import Tooltip from 'components/SimpleCommonComponents/Tooltip';
 
 import AnimatedEntry from 'components/AnimatedEntry';
 import Link from 'components/generic/Link';
