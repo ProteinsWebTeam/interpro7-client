bfc91480b836d02dd8cf423e2aba314d5fdec406
pesseat <Rupture26>
2017-12-05 10:38:11 +0000

    update position tooltip to top

--- src/components/SimpleCommonComponents/Tooltip/index.js
+++ src/components/SimpleCommonComponents/Tooltip/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tooltip } from 'react-tippy';
 
 const _Tooltip = ({ ...rest }) => (
-  <Tooltip animation="shift" arrow="true" position="bottom" {...rest} />
+  <Tooltip animation="shift" arrow="true" position="top" {...rest} />
 );
 _Tooltip.displayName = 'Tooltip';
 
