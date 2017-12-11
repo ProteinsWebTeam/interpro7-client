eaf6b5102d2fde16e162416556df6be681bba48b
pesseat <Rupture26>
2017-12-11 14:13:22 +0000

    Replace "Contributing signatures" wording by "Contributing entries", as requested

--- src/components/Entry/ContributingSignatures/index.js
+++ src/components/Entry/ContributingSignatures/index.js
@@ -69,7 +69,7 @@ SignatureLink.propTypes = {
 const ContributingSignatures = ({ contr } /*: {contr: Object} */) => (
   <div className={f('side-panel', 'margin-top-small', 'margin-bottom-large')}>
     <div className={f('md-icon-list-box', 'margin-bottom-large')}>
-      <h5>Contributing signatures</h5>
+      <h5>Contributing entries</h5>
       <ul className={f('md-list')}>
         {Object.entries(contr).map(([db, accessions]) => (
           <li key={db}>
