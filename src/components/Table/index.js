06450599ab84cdaaa72a0790fe3a98311fa8be2c
Aurélien Luciani <luciani@ebi.ac.uk>
2018-01-19 14:12:34 +0000

    well, actually…

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -139,12 +139,12 @@ export default class Table extends PureComponent /*:: <Props> */ {
                   columns={columns}
                   notFound={notFound}
                 />
-                <_Footer
-                  withPageSizeSelector={withPageSizeSelector}
-                  actualSize={actualSize}
-                  pagination={_query}
-                />
               </table>
+              <_Footer
+                withPageSizeSelector={withPageSizeSelector}
+                actualSize={actualSize}
+                pagination={_query}
+              />
             </div>
           </div>
         </div>
