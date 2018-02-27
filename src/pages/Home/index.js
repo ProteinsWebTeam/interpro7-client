b78c4a10c5f0de8808452657a9155a571e000f72
pesseat <Rupture26>
2018-02-27 10:52:35 +0000

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -452,6 +452,9 @@ class Home extends PureComponent {
                   <InterProGraphicAnim />
                 </Tooltip>
               </div>
+              {
+                //test
+              }
               <h3>Classification of protein families</h3>
               <Description textBlocks={[description]} />
             </div>
