1e99baa686ad3e3047960a1a43a6967c1a5fcfd0
pesseat <Rupture26>
2018-03-14 16:47:59 +0000

    PFAM MD text was missing

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -138,6 +138,11 @@ const memberDBs = [
                   US.`,
   },
   {
+    type: 'pfam',
+    title: 'PFAM',
+    description: `is a large collection of multiple sequence alignments and hidden Markov models covering many common protein domains. Pfam is based at EMBL-EBI, Hinxton, UK.`,
+  },
+  {
     type: 'pirsf',
     title: 'PIRSF',
     description: `protein classification system is a
