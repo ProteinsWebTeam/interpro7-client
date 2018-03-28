77b47466c58a931bad1439abbfa288e8da89d5d1
Aurélien Luciani <luciani@ebi.ac.uk>
2018-03-28 11:09:42 +0100

    added InterPro to MemberSymbol

--- src/components/Entry/MemberSymbol/index.js
+++ src/components/Entry/MemberSymbol/index.js
@@ -6,30 +6,33 @@ import ipro from 'styles/interpro-new.css';
 
 import uniqueId from 'utils/cheapUniqueId';
 
+import cn from 'classnames';
+
 const f = foundationPartial(ipro);
 
-const classNames = {
-  CATH: f('md-cg'),
-  CATHGENE3D: f('md-cg'),
-  CDD: f('md-cd'),
-  HAMAP: f('md-ha'),
-  MOBIDBLT: f('md-mo'),
-  PANTHER: f('md-pa'),
-  PFAM: f('md-pf'),
-  PIRSF: f('md-pi'),
-  PRINTS: f('md-pri'),
-  PRODOM: f('md-pro'),
-  PATTERNS: f('md-prpat'),
-  PROSITE: f('md-prpat'),
-  PROFILES: f('md-prpro'),
-  PROFILE: f('md-prpro'),
-  SFLD: f('md-sf'),
-  SMART: f('md-sm'),
-  SUPERFAMILIES: f('md-su'),
-  SSF: f('md-su'),
-  TIGRFAMS: f('md-ti'),
-  NEW: f('md-new'),
-};
+const classNames = new Map([
+  ['INTERPRO', f('md-ip')],
+  ['CATH', f('md-cg')],
+  ['CATHGENE3D', f('md-cg')],
+  ['CDD', f('md-cd')],
+  ['HAMAP', f('md-ha')],
+  ['MOBIDBLT', f('md-mo')],
+  ['PANTHER', f('md-pa')],
+  ['PFAM', f('md-pf')],
+  ['PIRSF', f('md-pi')],
+  ['PRINTS', f('md-pri')],
+  ['PRODOM', f('md-pro')],
+  ['PATTERNS', f('md-prpat')],
+  ['PROSITE', f('md-prpat')],
+  ['PROFILES', f('md-prpro')],
+  ['PROFILE', f('md-prpro')],
+  ['SFLD', f('md-sf')],
+  ['SMART', f('md-sm')],
+  ['SUPERFAMILIES', f('md-su')],
+  ['SSF', f('md-su')],
+  ['TIGRFAMS', f('md-ti')],
+  ['NEW', f('md-new')],
+]);
 
 const MemberSymbol = (
   { type, className = '' } /*: { type: string, className?: string }*/,
@@ -41,7 +44,7 @@ const MemberSymbol = (
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 200 200"
         id={`md-${type}`}
-        className={`${className} ${classNames[type.toUpperCase()]}`}
+        className={cn(className, classNames.get(type.toUpperCase()))}
       >
         <defs>
           <clipPath id={`clip-${id}`}>
