467e129d7f3a8ef8e36012af65e56c21b402aeeb
pesseat <Rupture26>
2018-04-03 16:27:13 +0100

    Add mapping for icon, color, and lineage

--- src/components/Table/views/Grid/index.js
+++ src/components/Table/views/Grid/index.js
@@ -18,31 +18,6 @@ import local from './style.css';
 
 const f = foundationPartial(local, fonts);
 
-const lut = new Map([
-  ['3702', { shortname: 'Mouse-ear cress' }],
-  ['6239', { shortname: 'C. elegans', icon: 'W' }],
-  ['7227', { shortname: 'Fruit fly', icon: 'F' }],
-  ['7739', { shortname: 'Amphioxus' }],
-  ['7955', { shortname: 'Zebrafish', icon: 'Z' }],
-  ['8078', { shortname: 'Mummichog' }],
-  ['8355', { shortname: 'African clawed frog', icon: 'f' }],
-  ['9031', { shortname: 'Chicken', icon: 'k' }],
-  ['9430', { shortname: 'Vampire bat', icon: '(' }],
-  ['9544', { shortname: 'Rhesus macaque', icon: 'r' }],
-  ['9601', { shortname: 'Sumatran orangutan', icon: '*' }],
-  ['9606', { shortname: 'Human', icon: 'H' }],
-  ['9615', { shortname: 'Dog', icon: 'd' }],
-  ['9823', { shortname: 'Pig', icon: 'p' }],
-  ['9913', { shortname: 'Bovine', icon: 'C' }],
-  ['10090', { shortname: 'Mouse', icon: 'M' }],
-  ['10116', { shortname: 'Rat', icon: 'R' }],
-  ['31033', { shortname: 'Fugu rubripes', icon: 'E' }],
-  ['35525', { shortname: 'Daphnia magna' }],
-  ['39947', { shortname: 'Rice', icon: '6' }],
-  ['282301', { shortname: 'Macrostomum lignano' }],
-  ['559292', { shortname: 'Baker s yeast', icon: 'Y' }],
-]);
-
 const getUrlFor = accession =>
   createSelector(
     state => state.settings.api,
@@ -59,19 +34,284 @@ const getUrlFor = accession =>
           }),
       }),
   );
+
 class Lineage extends PureComponent {
   render() {
-    const { entryDB, metadata } = this.props;
-    return <div>test</div>;
+    const { metadata, data: { loading, payload } } = this.props;
+    const linetree = `${loading ? 0 : payload.metadata.lineage}`;
+    let superkingdom = null;
+    let nodespot = null;
+
+    if (linetree.includes(' 2759 ')) {
+      superkingdom = 'Eukaryota';
+    } else if (linetree.includes(' 2157 ')) {
+      superkingdom = 'Archaea';
+    } else if (linetree.includes(' 10239 ')) {
+      superkingdom = 'Viruses';
+    } else if (linetree.includes(' 12884 ')) {
+      superkingdom = 'Viroids';
+    } else if (linetree.includes(' 2 ')) {
+      superkingdom = 'Bacteria';
+    } else {
+      superkingdom = 'N/A';
+    }
+
+    if (linetree.includes(' 1224 ')) {
+      nodespot = 'Proteobacteria';
+    } else if (linetree.includes(' 201174 ')) {
+      nodespot = 'Actinobacteria';
+    } else if (linetree.includes(' 203691 ')) {
+      nodespot = 'Spirochaetes';
+    } else if (linetree.includes(' 203682 ')) {
+      nodespot = 'Planctomycetes';
+    } else if (linetree.includes(' 1239 ')) {
+      nodespot = 'Firmicutes';
+    } else if (linetree.includes(' 976 ')) {
+      nodespot = 'Bacteroidetes';
+    } else if (linetree.includes(' 40674 ')) {
+      nodespot = 'Mammalia';
+    } else if (linetree.includes(' 6656 ')) {
+      nodespot = 'Arthropoda';
+    } else if (linetree.includes(' 33090 ')) {
+      nodespot = 'Viridiplantae';
+    } else if (linetree.includes(' 6231 ')) {
+      nodespot = 'Nematoda';
+    } else if (linetree.includes(' 8782 ')) {
+      nodespot = 'Birds';
+    } else if (linetree.includes(' 4751 ')) {
+      nodespot = 'Fungi';
+    } else if (linetree.includes(' 32561 ')) {
+      nodespot = 'Sauria';
+    } else if (linetree.includes('7898 ')) {
+      nodespot = 'Fish';
+    } else {
+      /*Actinopterygii - 89593 craniata too general*/
+      nodespot = 'N/A';
+    }
+
+    return (
+      <Tooltip title={`Lineage: ${loading ? 0 : payload.metadata.lineage} `}>
+        {superkingdom} ({nodespot})
+      </Tooltip>
+    );
   }
 }
+
+class SpeciesIcon extends PureComponent {
+  render() {
+    const { metadata, data: { loading, payload } } = this.props;
+    const linetree = `${loading ? 0 : payload.metadata.lineage}`;
+    let icon = null;
+    let nodecolor = null;
+    // key species in Interpro & EBI
+    if (linetree.includes(' 7227 ') || linetree.includes(' 27457 ')) {
+      icon = 'F';
+    } else if (linetree.includes(' 7215 ')) {
+      /*drosophila melanogaster and Bactrocera dorsalis*/
+      icon = 'F';
+    } else if (linetree.includes(' 121224 ')) {
+      /***Node drosophila*/
+      icon = '4';
+    } else if (linetree.includes(' 34735 ')) {
+      /* Pediculus humanus subsp. corporis (louse)*/
+      icon = '$';
+    } else if (linetree.includes(' 6855 ')) {
+      /***Node Apoidea (bees)*/
+      icon = 's';
+    } else if (linetree.includes(' 6935 ')) {
+      /***Node Scorpiones (Scorpion)*/
+      icon = '&';
+    } else if (linetree.includes(' 6448 ')) {
+      /***Node Ixodida (Ticks)*/
+      icon = "'";
+    } else if (linetree.includes(' 28376 ')) {
+      /***Node Gastropoda (Snail)*/
+      icon = '7';
+    } else if (linetree.includes(' 9681 ')) {
+      /***Node anolis*/
+      icon = 'A';
+    } else if (linetree.includes(' 9606 ')) {
+      /***Node felidae*/
+      icon = 'H';
+    } else if (linetree.includes(' 3702 ')) {
+      /*homo*/
+      icon = 'B';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 6239 ')) {
+      /*Mouse-ear cress*/
+      icon = 'W';
+    } else if (linetree.includes(' 9031 ')) {
+      /*Caenorhabditis elegans*/
+      icon = 'k';
+    } else if (linetree.includes(' 9922 ')) {
+      /* Chicken*/
+      icon = 'm';
+    } else if (linetree.includes(' 55153 ')) {
+      /*** Node Capra (goat)*/
+      icon = 'I';
+    } else if (linetree.includes(' 9723 ')) {
+      /*** Node Sciuridae (Squirels)*/
+      icon = 'D';
+    } else if (linetree.includes(' 8292 ')) {
+      /*** Node Platanistidae (Dolphin)*/
+      icon = 'f';
+    } else if (linetree.includes(' 8782 ')) {
+      /*** Node Amphibia */
+      icon = 'n';
+    } else if (linetree.includes(' 7157 ')) {
+      /*** Node Bird (Finch)*/
+      icon = '1';
+    } else if (linetree.includes(' 6656 ')) {
+      /*** subNode Culicidae (Mosquito) */
+      icon = 'S';
+    } else if (linetree.includes(' 1639119 ')) {
+      /*** Node Arthropoda (Spider)*/
+      icon = '@';
+    } else if (linetree.includes(' 7955 ')) {
+      /*** Node Plasmodium (Plasmodiidae)*/
+      icon = 'Z';
+    } else if (linetree.includes(' 31031 ')) {
+      /* Zebrafish */
+      icon = 'E';
+    } else if (linetree.includes(' 7898 ')) {
+      /* Tetraodontidae (Pufferfish) */
+      icon = 'Z';
+    } else if (linetree.includes(' 10090 ')) {
+      /*** Node Fish (7898 Actinopterygii NOT 7776 Gnathostomata includes mammals -  NOT 7777 Chondrichthyes)*/
+      icon = 'M';
+    } else if (linetree.includes(' 10088 ')) {
+      /* Mouse*/
+      icon = 'M';
+    } else if (linetree.includes(' 9368 ')) {
+      /*** Node Mouse (?)*/
+      icon = 'o';
+    } else if (linetree.includes(' 10116 ')) {
+      /*** Node Erinaceidae (Hedgehog)*/
+      icon = 'R';
+    } else if (linetree.includes(' 9615 ')) {
+      /* Rat*/
+      icon = 'd';
+    } else if (linetree.includes(' 9913 ')) {
+      /* Dog*/
+      icon = 'C';
+    } else if (linetree.includes(' 9783 ')) {
+      /* Bovine*/
+      icon = 'e';
+    } else if (linetree.includes(' 9940 ')) {
+      /* Indian Elephant*/
+      icon = 'x';
+    } else if (linetree.includes(' 9986 ')) {
+      /* Sheep */
+      icon = 't';
+    } else if (linetree.includes(' 9821 ')) {
+      /* Rabbit */
+      icon = 'p';
+    } else if (linetree.includes(' 10141 ')) {
+      /*  Suidae (Pig)*/
+      icon = 'g';
+    } else if (linetree.includes(' 126299 ')) {
+      /* Cavia porcellus (Guinea pig)*/
+      icon = '9';
+    } else if (linetree.includes(' 9555 ')) {
+      /*  Monodelphis*/
+      icon = '8';
+    } else if (linetree.includes(' 9596 ')) {
+      /* Papio anubis (Olive baboon)*/
+      icon = 'i';
+    } else if (linetree.includes(' 9527 ')) {
+      /*** Node Pan troglodytes (Chimpanzee)*/
+      icon = 'r';
+    } else if (linetree.includes(' 9430 ')) {
+      /*** Node Cercopithecidae (Monkeys) to check*/
+      icon = '(';
+    } else if (linetree.includes(' 9397 ')) {
+      /* Vampire bat*/
+      icon = '(';
+    } else if (linetree.includes(' 9544 ')) {
+      /*** Node bat Chiroptera*/
+      icon = 'r';
+    } else if (linetree.includes(' 9599 ')) {
+      /* Rhesus macaque*/
+      icon = '*';
+    } else if (linetree.includes(' 9592 ')) {
+      /* Node pongo (orangutan)*/
+      icon = 'G';
+    } else if (linetree.includes(' 31033 ')) {
+      /*** Node Gorilla*/
+      icon = 'E';
+    } else if (linetree.includes(' 4932 ') || linetree.includes('284812')) {
+      /* Fugu rubripes*/
+      icon = 'Y';
+      nodecolor = '#5bc0de';
+    } else if (linetree.includes(' 2157 ')) {
+      /* Saccharomyces cerevisiae (Baker's yeast) and Fission yeast*/
+      icon = 'Y';
+      nodecolor = '#5bc0de';
+    } else if (linetree.includes(' 2 ')) {
+      /* Archea*/
+      icon = 'L';
+      nodecolor = '#5bc0de';
+    } else if (linetree.includes(' 10239 ')) {
+      /*** Node Bacteria (Ecoli)*/
+      icon = 'v';
+      nodecolor = '#5bc0de';
+    } else if (linetree.includes(' 4751 ')) {
+      /*** Node Virus */
+      icon = 'u';
+      nodecolor = '#5bc0de';
+    } else if (linetree.includes(' 4527 ')) {
+      /*** Node Fungus*/
+      icon = '6';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 49274 ')) {
+      /*** Node Rice (Oriza sativa)*/
+      icon = ')';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 4512 ')) {
+      /*** Node Lycopersicon (Tomatoes)*/
+      icon = '5';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 3700 ')) {
+      /*** Node Barley (Hordeum)*/
+      icon = 'B';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 3603 ')) {
+      /*** Node Brassicaceae ()*/
+      icon = 'O';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 1462606 ')) {
+      /*** Node Vitis (grape)*/
+      icon = '^';
+      nodecolor = '#5cb85c';
+    } else if (linetree.includes(' 33090 ')) {
+      /*** Node Soja (Glycine max (Soybean))*/
+      icon = '.';
+      nodecolor = '#5cb85c';
+    } else {
+      /*** Node viridiplantae*/
+
+      icon = '.';
+    }
+
+    return (
+      <Tooltip title="This is a ... icon">
+        {' '}
+        <span
+          style={{ color: nodecolor }} /*bleu #*/
+          className={f('small', 'icon', 'icon-species')}
+          data-icon={icon}
+        />
+      </Tooltip>
+    );
+  }
+}
+
 class SummaryCounter extends PureComponent {
   render() {
     const { entryDB, metadata, data: { loading, payload } } = this.props;
     return (
       <div className={f('card-item-m', 'card-sum-info', 'label-off')}>
         <div className={f('count-entries')}>
-          {loading ? 0 : payload.metadata.lineage}
           <Tooltip
             title={`${
               loading ? 0 : payload.metadata.counters.entries
@@ -87,7 +327,7 @@ class SummaryCounter extends PureComponent {
               }}
             >
               {
-                // get the db
+                // get the db icon
               }
               <MemberSymbol type={entryDB} className={f('md-small')} />
               <NumberComponent
@@ -193,11 +433,6 @@ class GridView extends PureComponent {
     dataTable: T.array,
   };
 
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-
   render() {
     const { dataTable, entryDB } = this.props;
     return (
@@ -206,6 +441,13 @@ class GridView extends PureComponent {
           const SummaryCounterWithData = loadData(
             getUrlFor(`${metadata.accession}`),
           )(SummaryCounter);
+          const LineageWithData = loadData(getUrlFor(`${metadata.accession}`))(
+            Lineage,
+          );
+          const SpeciesIconWithData = loadData(
+            getUrlFor(`${metadata.accession}`),
+          )(SpeciesIcon);
+
           return (
             <div className={f('card-flex-container')} key={metadata.accession}>
               <div className={f('card-item')}>
@@ -221,53 +463,11 @@ class GridView extends PureComponent {
                       },
                     }}
                   >
-                    {// E.Coli
-                    metadata.accession === 562 ||
-                    metadata.accession === 511693 ||
-                    metadata.accession === 1248823 ||
-                    metadata.accession === 1400022 ||
-                    metadata.accession === 1438695 ||
-                    metadata.accession === 1444037 ||
-                    metadata.accession === 1444161 ||
-                    metadata.accession === 1444228 ||
-                    metadata.accession === 1444258 ||
-                    metadata.accession === 1446533 ||
-                    metadata.accession === 1446549 ||
-                    metadata.accession === 1446596 ||
-                    metadata.accession === 1446704 ||
-                    metadata.accession === 1446753 ? (
-                      <Tooltip title="This is an E.Coli icon">
-                        {' '}
-                        <span
-                          style={{ color: '#5bc0de' }}
-                          className={f('small', 'icon', 'icon-species')}
-                          data-icon="&#x4c;"
-                        />
-                      </Tooltip>
-                    ) : (
-                      ''
-                    )}
+                    {
+                      // get the species icon
+                    }
 
-                    {// HUMAN
-                    metadata.accession === 9606 ||
-                    metadata.accession === 63221 ? (
-                      <Tooltip title="This is a human icon">
-                        {' '}
-                        <span
-                          style={{ color: '#e47471' }}
-                          className={f('small', 'icon', 'icon-species')}
-                          data-icon="H"
-                        />
-                      </Tooltip>
-                    ) : (
-                      <Tooltip title="Default species icon">
-                        <span
-                          style={{ color: '#ddd' }}
-                          className={f('small', 'icon', 'icon-conceptual')}
-                          data-icon="&#x6f;"
-                        />
-                      </Tooltip>
-                    )}
+                    <SpeciesIconWithData />
 
                     <h6>{metadata.name}</h6>
                   </Link>
@@ -280,10 +480,8 @@ class GridView extends PureComponent {
                 </div>
 
                 <div className={f('card-item-b')}>
-                  <div>
-                    {metadata.parent}
-                    Lineage: <Lineage entryDB={entryDB} metadata={metadata} />
-                  </div>
+                  <LineageWithData />
+
                   <div>Tax ID: {metadata.accession}</div>
                 </div>
               </div>
