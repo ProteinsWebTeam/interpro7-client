25f0fbceb64cd0e285a36f67290b7d9a14777f2e
Aurélien Luciani <luciani@ebi.ac.uk>
2018-05-16 08:28:01 +0100

    refactoring

--- src/components/About/Privacy/index.js
+++ src/components/About/Privacy/index.js
@@ -1,22 +1,12 @@
 // @flow
 import React, { PureComponent } from 'react';
 
-import UserId from 'components/UserId';
-
 export default class Privacy extends PureComponent /*:: <{}> */ {
   render() {
     return (
       <section>
         <h4>Privacy</h4>
         <p>lorem ipsum</p>
-        <h5>UUID</h5>
-        <p>
-          A unique identifier is associated to this session for analytics
-          purposes. It is not linked to any personal data, it is generated on
-          this browser and there is no way for us to link it back to an
-          individual. You can reset it at any time.
-        </p>
-        <UserId />
       </section>
     );
   }
