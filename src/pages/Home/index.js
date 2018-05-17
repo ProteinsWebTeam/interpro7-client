a3229898f41024c9dda179efa043618954fcbaba
pesseat <Rupture26>
2018-05-17 15:40:21 +0100

    Update introduction for the website as suggested in feedback

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -413,14 +413,8 @@ class InterProGraphicAnim extends PureComponent {
 }
 
 const description = `
-InterPro provides functional analysis of proteins by classifying them into
-families and predicting domains and important sites. We combine protein
-signatures from a number of member databases into a single searchable
-resource, capitalising on their individual strengths to produce a powerful
-integrated database and diagnostic tool. To classify proteins in this way,
-InterPro uses predictive models, known as signatures, provided by several
-different databases (referred to as member databases) that make up the
-InterPro consortium.`.trim();
+InterPro provides functional analysis of proteins by classifying them into families and predicting domains and important sites. To classify proteins in this way, InterPro uses predictive models, known as signatures, provided by several different databases (referred to as member databases) that make up the InterPro consortium. We combine protein signatures from these member databases into a single searchable resource, capitalising on their individual strengths to produce a powerful integrated database and diagnostic tool.
+`.trim();
 
 class Home extends PureComponent {
   static propTypes = {
