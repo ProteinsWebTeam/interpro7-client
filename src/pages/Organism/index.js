483d6d9cc5e1b083973851c327c8138a60788d4b
pesseat <Rupture26>
2018-05-30 17:33:32 +0100

    Import ebiGLobalStyle to make the page fit with grid and use full width

--- src/pages/Organism/index.js
+++ src/pages/Organism/index.js
@@ -52,11 +52,11 @@ import { getUrlForMeta } from '../../higherOrder/loadData/defaults';
 
 import { foundationPartial } from 'styles/foundation';
 
+import ebiGlobalStyles from 'ebi-framework/css/ebi-global.css';
 import pageStyle from '../style.css';
-import styles from 'styles/blocks.css';
 import fonts from 'EBI-Icon-fonts/fonts.css';
 
-const f = foundationPartial(pageStyle, styles, fonts);
+const f = foundationPartial(pageStyle, ebiGlobalStyles, fonts);
 
 const EntryAccessionsRenderer = taxId => (
   <ProteinFile taxId={`${taxId}`} type="entry-accession" />
@@ -98,7 +98,7 @@ class Overview extends PureComponent {
       data: { payload = defaultPayload },
     } = this.props;
     return (
-      <ul className={f('card')}>
+      <ul>
         {Object.entries(payload.organisms || {}).map(([name, count]) => (
           <li key={name}>
             <Link
