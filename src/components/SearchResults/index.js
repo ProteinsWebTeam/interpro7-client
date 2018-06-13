4821eb9292b98df3f5450ef7a625c9a3fd860e3a
Aurélien Luciani <luciani@ebi.ac.uk>
2018-06-13 08:52:33 +0100

    revert change in URL export for EBI Search results

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -82,7 +82,7 @@ class SearchResults extends PureComponent {
             <Link
               disabled={!url}
               target="_blank"
-              href={url.replace(/start=\d+/, 'start=0')}
+              href={url}
               download={`SearchResults-${searchValue}.json`}
             >
               JSON
