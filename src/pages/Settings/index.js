f4af4bd9ac6191fb35cd4d86b8c9364f55407c64
pesseat <Rupture26>
2018-06-26 15:26:11 +0100

    Import the developper information at bottom of settings and import the ebiglobalstyle to make the page fit in the grid

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -5,6 +5,12 @@ import { createSelector } from 'reselect';
 
 import { DEV } from 'config';
 
+// Generate async components
+const Advanced = loadable({
+  loader: () =>
+    import(/* webpackChunkName: "about-advanced" */ 'components/About/Advanced'),
+});
+
 import noop from 'lodash-es/noop';
 
 import {
@@ -16,11 +22,12 @@ import { changeSettings, resetSettings } from 'actions/creators';
 
 import { foundationPartial } from 'styles/foundation';
 
+import ebiGlobalStyles from 'ebi-framework/css/ebi-global.css';
 import theme from 'styles/theme-interpro.css';
 import local from './styles.css';
 import loadable from 'higherOrder/loadable';
 
-const f = foundationPartial(theme, local);
+const f = foundationPartial(ebiGlobalStyles, theme, local);
 
 const NavigationSettings = ({ navigation: { pageSize } }) => (
   <form data-category="navigation">
@@ -313,8 +320,11 @@ class Settings extends PureComponent {
               navigation={navigation}
               handleChange={changeSettings}
             />
+
             <UISettings ui={ui} />
+
             <CacheSettings cache={cache} />
+
             <APIEndpointSettings category="api" endpointDetails={api}>
               API Settings {!DEV && '(modification temporarily disabled)'}
             </APIEndpointSettings>
@@ -326,10 +336,13 @@ class Settings extends PureComponent {
               InterProScan Settings{' '}
               {!DEV && '(modification temporarily disabled)'}
             </IPScanEndpointSettings>
+
             <button onClick={this._handleReset} className={f('button')}>
               Reset settings to default values
             </button>
           </section>
+
+          <Advanced />
         </div>
       </div>
     );
