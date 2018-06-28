881c399b07b84e88fe58795e79268d132f79e836
Aurélien Luciani <luciani@ebi.ac.uk>
2018-06-28 14:33:45 +0100

    added toast when new version of website is available

--- src/offline.js
+++ src/offline.js
@@ -1,7 +1,13 @@
 // @flow
 import * as runtime from 'offline-plugin/runtime';
 
-export default () => {
+import id from 'utils/cheap-unique-id';
+
+import { addToast } from 'actions/creators';
+
+/*:: import type Store from 'redux'; */
+
+export default ({ dispatch } /*: Store */) => {
   runtime.install({
     onUpdating: () => {
       console.log('SW Event:', 'onUpdating');
@@ -13,8 +19,23 @@ export default () => {
     },
     onUpdated: () => {
       console.log('SW Event:', 'onUpdated');
-      // Reload the webpage to load into the new version
-      window.location.reload();
+      // Reload the webpage to load into the new version if user wants it
+      dispatch(
+        addToast(
+          {
+            title: 'New version ready',
+            body:
+              'A new version of this website is ready, reload this page now to use it',
+            action: {
+              text: 'Load new version',
+              fn() {
+                window.location.reload();
+              },
+            },
+          },
+          id(),
+        ),
+      );
     },
 
     onUpdateFailed: () => {
