da8ffc32322bb072e89da0a784b00a5b0f503126
pesseat <Rupture26>
2018-07-10 10:54:57 +0100

    When listing signatures , replace <br> by comma and add comma only, when more than one signature

--- src/components/Entry/ContributingSignatures/index.js
+++ src/components/Entry/ContributingSignatures/index.js
@@ -40,7 +40,6 @@ const SignatureLink = ({ accession, db, label }) => (
   >
     <small>
       <Tooltip title={`${label} (${accession})`}>{accession || label}</Tooltip>
-      <br />
     </small>
   </Link>
 );
@@ -62,12 +61,13 @@ const ContributingSignatures = ({ contr } /*: {contr: Object} */) => {
               <MemberSymbol type={db} className={f('md-small')} />
               <div>
                 <span className={f('db-name')}>{db}: </span>{' '}
-                {Object.entries(signatures).map(([accession, name]) => (
+                {Object.entries(signatures).map(([accession, name], index) => (
                   <React.Fragment key={accession}>
                     <SchemaOrgData
                       data={{ db, name: accession }}
                       processData={schemaProcessData}
                     />
+                    {index ? ', ' : ''}
                     <SignatureLink db={db} accession={accession} label={name} />
                   </React.Fragment>
                 ))}
