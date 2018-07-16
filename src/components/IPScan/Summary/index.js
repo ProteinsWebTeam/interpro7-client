a1cabbd7f7582becb440118a43480926a77e1d4c
Aurélien Luciani <luciani@ebi.ac.uk>
2018-07-16 09:13:40 +0100

    whitelist of libraries to not use as DBs in IPScan results

--- src/components/IPScan/Summary/index.js
+++ src/components/IPScan/Summary/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import T from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
+import flattenDeep from 'lodash-es/flattenDeep';
 
 import GoTerms from 'components/GoTerms';
 import Length from 'components/Protein/Length';
@@ -10,7 +11,7 @@ import Title from 'components/Title';
 import { DomainOnProteinWithoutMergedData } from 'components/Related/DomainsOnProtein';
 import Actions from 'components/IPScan/Actions';
 
-import flattenDeep from 'lodash-es/flattenDeep';
+import { NOT_MEMBER_DBS } from 'menuConfig';
 
 import f from 'styles/foundation';
 
@@ -99,7 +100,7 @@ class SummaryIPScanJob extends PureComponent /*:: <Props> */ {
         ],
         score: match.score,
       };
-      if (library === 'PHOBIUS' || library === 'MOBIDB_LITE') {
+      if (NOT_MEMBER_DBS.has(library)) {
         processedMatch.accession += ` (${mergedData.predictions.length + 1})`;
         mergedData.predictions.push(processedMatch);
       } else if (match.signature.entry) {
