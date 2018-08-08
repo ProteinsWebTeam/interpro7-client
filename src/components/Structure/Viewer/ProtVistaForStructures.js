a644c212fbf65933c74f5700e66c9b2ce2fa9eb9
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-08-08 12:37:10 +0100

    linting fixes

--- src/components/Structure/Viewer/ProtVistaForStructures.js
+++ src/components/Structure/Viewer/ProtVistaForStructures.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import T from 'prop-types';
 import loadData from 'higherOrder/loadData';
 import { createSelector } from 'reselect';
