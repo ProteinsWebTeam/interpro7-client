52427010764d64add60571d73d971d056affd8e3
Aurélien Luciani <luciani@ebi.ac.uk>
2018-09-06 10:54:10 +0100

    added toast when coming from InterPro6

--- src/store/enhancer/location-middleware/index.js
+++ src/store/enhancer/location-middleware/index.js
@@ -4,9 +4,10 @@
 /*:: import type history from 'history/createBrowserHistory'; */
 /*:: declare var ga: (...args: Array<string>) => void; */
 import { format } from 'url';
+import { sleep } from 'timing-functions/src';
 
 import { NEW_CUSTOM_LOCATION } from 'actions/types';
-import { customLocationChangeFromHistory } from 'actions/creators';
+import { customLocationChangeFromHistory, addToast } from 'actions/creators';
 
 import descriptionToPath from 'utils/processDescription/descriptionToPath';
 import autoScroll from 'utils/auto-scroll';
@@ -37,13 +38,35 @@ const middleware = (history /*: history */) /*: Middleware<*, *, *> */ => ({
     ga('send', 'pageview');
   });
 
-  const historyDispatch = ({ customLocation, replace, state }) =>
+  const historyDispatch = ({ customLocation, replace, state }) => {
+    const { from_interpro6: fromIP6, ...query } = customLocation.search || {};
+    if (typeof fromIP6 !== 'undefined') {
+      // eslint-disable-next-line no-magic-numbers
+      sleep(1000).then(() => {
+        dispatch(
+          addToast(
+            {
+              title: 'Welcome on the new InterPro website 👋',
+              body:
+                'Explore the website and try our new features, but keep in mind that this is a ⚠️beta⚠️ and things can break. If they do, send us a ticket',
+              link: {
+                href: 'https://www.ebi.ac.uk/support/interpro',
+                children: 'Click here to submit a ticket 📩',
+              },
+              ttl: 15000, // eslint-disable-line no-magic-numbers
+            },
+            'welcome_from_interpro6',
+          ),
+        );
+      });
+    }
     history[replace ? 'replace' : 'push']({
       pathname: descriptionToPath(customLocation.description),
-      search: format({ query: customLocation.search }),
+      search: format({ query }),
       hash: customLocation.hash,
       state: { customLocation, state },
     });
+  };
 
   // Just run once on startup
   historyDispatch({
