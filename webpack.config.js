bc154618835041d9a5e91f103c5b635ea1c82d4d
Aurélien Luciani <luciani@ebi.ac.uk>
2018-09-12 12:25:56 +0100

    production bugfix: added main css to html template

--- webpack.config.js
+++ webpack.config.js
@@ -53,11 +53,6 @@ const cssSettings = mode => ({
 
 const publicPath = websiteURL.pathname || '/interpro/';
 
-const miniCssExtractPlugin = new MiniCssExtractPlugin({
-  filename: path.join('css', '[name].[contenthash:3].css'),
-  chunkFilename: path.join('css', '[id].[contenthash:3].css'),
-});
-
 const getHTMLWebpackPlugin = mode =>
   new HTMLWebpackPlugin({
     title: pkg.name,
@@ -67,322 +62,321 @@ const getHTMLWebpackPlugin = mode =>
 
 const legacyModuleSplitPlugin = new LegacyModuleSplitPlugin();
 
-const getConfigFor = (env, mode, module = false) => ({
-  // NAME
-  name: module ? 'module' : 'legacy',
-  // MODE
-  mode,
-  // OUTPUT
-  output: {
-    path: path.resolve('dist'),
-    publicPath,
-    filename: path.join(
-      'js',
-      `[id].${module ? 'module' : 'legacy'}.[name].[hash:3].js`
-    ),
-    chunkFilename: path.join(
-      'js',
-      `[id].${module ? 'module' : 'legacy'}.[name].[chunkhash:3].js`
-    ),
-    globalObject: 'self',
-  },
-  // RESOLVE
-  resolve: {
-    modules: [path.resolve('.', 'src'), 'node_modules'],
-    extensions: ['.js', '.json', '.worker.js'],
-  },
-  // MODULE
-  module: {
-    rules: [
-      {
-        test: /\.worker\.js$/i,
-        use: [
-          {
-            loader: 'worker-loader',
-            options: {
-              name: path.join(
-                'js',
-                'workers',
-                `[folder].${
-                  module ? 'module' : 'legacy'
-                }.[name].[hash:3].worker.js`
-              ),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/i,
-        include: [
-          path.resolve('src'),
-          path.resolve('node_modules', 'lodash-es'),
-          // path.resolve('node_modules', 'color-hash'),
-          path.resolve('node_modules', 'timing-functions'),
-          /protvista/i,
-          path.resolve('node_modules', 'd3'),
-          path.resolve('node_modules', 'data-loader'),
-          path.resolve('node_modules', 'interpro-components'),
-          path.resolve('node_modules', 'lit-html'),
-          path.resolve('node_modules', 'pdb-web-components'),
-        ],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/env',
-                  {
-                    modules: false,
-                    loose: true,
-                    useBuiltIns: 'usage',
-                    targets: module
-                      ? { esmodules: true }
-                      : { browsers: '> 0.25%' },
-                  },
-                ],
-                ['@babel/react', { development: mode === 'development' }],
-              ],
-              plugins: [
-                '@babel/plugin-syntax-dynamic-import',
-                ['@babel/plugin-proposal-class-properties', { loose: true }],
-              ],
-              env: {
-                dev: {
-                  // better sourcemaps for JSX code
-                  plugins: ['transform-react-jsx-source'],
-                },
-                production: {
-                  plugins: [
-                    // optimisations for react
-                    'babel-plugin-transform-react-remove-prop-types',
-                    'babel-plugin-transform-react-constant-elements',
-                    'babel-plugin-transform-react-inline-elements',
-                  ],
-                },
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(txt|fast[aq])/i,
-        use: [
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      },
-      {
-        test: /\.yml$/i,
-        use: [
-          {
-            loader: 'json-loader',
-          },
-          {
-            loader: 'yaml-loader',
-          },
-        ],
-      },
-      {
-        test: /((LiteMol-plugin-blue)|(LiteMol-plugin-light)|(LiteMol-plugin)|(tippy)|(clanviewer)|(ebi-global)|(interpro-new))\.css$/i,
-        use: [
-          {
-            loader:
-              mode === 'production'
-                ? MiniCssExtractPlugin.loader
-                : 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: Object.assign({}, cssSettings(mode), {
-              localIdentName: '[local]',
-            }),
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [postCSSImport, cssNext()],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader:
-              mode === 'production'
-                ? MiniCssExtractPlugin.loader
-                : 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: cssSettings(mode),
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [postCSSImport, cssNext()],
-            },
-          },
-        ],
-        exclude: /((LiteMol-plugin-blue)|(LiteMol-plugin-light)|(LiteMol-plugin)|(tippy)|(clanviewer)|(ebi-global)|(interpro-new))\.css$/i,
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          {
-            loader:
-              mode === 'production'
-                ? MiniCssExtractPlugin.loader
-                : 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: Object.assign({}, cssSettings(mode), {
-              localIdentName: '[local]',
-            }),
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: path.join(
-                'assets',
-                'images',
-                '[name].[hash:base62:3].[ext]'
-              ),
-              limit: 1 * kB,
-            },
-          },
-          {
-            loader: 'img-loader',
-            options: {
-              enabled: mode === 'production',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: path.join(
-                'assets',
-                'fonts',
-                '[name].[hash:base62:3].[ext]'
-              ),
-              limit: 1 * kB,
-              mimetype: 'application/font-woff',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: path.join(
-                'assets',
-                'fonts',
-                '[name].[hash:base62:3].[ext]'
-              ),
-            },
-          },
-        ],
-      },
-    ],
-  },
-  // PLUGINS
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        STAGING: JSON.stringify(!!env.staging),
-      },
-      'process.info': JSON.stringify({
-        git: buildInfo.git,
-        build: buildInfo.build,
-      }),
-    }),
-    mode === 'production' ? miniCssExtractPlugin : null,
-    mode === 'production'
-      ? new (require('offline-plugin'))({
-          caches: {
-            main: [
-              new RegExp(
-                `(js|css)/[^/]*${module ? 'module' : 'legacy'}[^/]*.\\1$`,
-                'i'
-              ),
-            ],
-            additional: [/\.(worker\.js)$/i],
-            optional: [/\.(eot|ttf|woff|svg|ico|png|jpe?g)$/i],
-          },
-          // TODO: check a way to use it without affecting /api
-          // appShell: publicPath,
-          AppCache: false,
-          // TODO: Check whats the best way to do this autoupdate.
-          // autoUpdate: 60000,
-          ServiceWorker: {
-            output: `sw.${module ? 'module' : 'legacy'}.js`,
-            events: true,
-          },
-          safeToUseOptionalCaches: true,
-          excludes: ['**/.*', '**/*.{map,br,gz}'],
-        })
-      : null,
-    // Custom plugin to split codebase into legacy/modern bundles,
-    // depends on HTMLWebpackPlugin
-    legacyModuleSplitPlugin,
-    // GZIP compression
-    mode === 'production'
-      ? new (getCompressionPlugin())({
-          asset: '[path].gz[query]',
-          test: /\.(js|css|html|svg)$/i,
-          cache: true,
-          algorithm(buffer, options, callback) {
-            require('node-zopfli-es').gzip(buffer, options, callback);
-          },
-        })
-      : null,
-    // Brotli compression
-    mode === 'production'
-      ? new (getCompressionPlugin())({
-          asset: '[path].br[query]',
-          test: /\.(js|css|html|svg)$/i,
-          cache: true,
-          algorithm(buffer, _, callback) {
-            require('iltorb').compress(
-              buffer,
-              {
-                mode: 1, // text
-                quality: 11, // goes from 1 (but quick) to 11 (but slow)
-              },
-              callback
-            );
-          },
-        })
-      : null,
-  ].filter(Boolean),
-  // optimization.minimize: mode === 'production',
-  devtool: ((mode, env) => {
-    if (mode === 'development') return 'cheap-module-source-map';
-    if (env.staging) return 'source-map';
-    return false;
-  })(mode, env),
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: path.join('css', `[name].[contenthash:3].css`),
+  chunkFilename: path.join('css', `[id].[contenthash:3].css`),
 });
+
+const getConfigFor = (env, mode, module = false) => {
+  const name = module ? 'module' : 'legacy';
+
+  return {
+    // NAME
+    name,
+    // MODE
+    mode,
+    // OUTPUT
+    output: {
+      path: path.resolve('dist'),
+      publicPath,
+      filename: path.join('js', `[id].${name}.[name].[hash:3].js`),
+      chunkFilename: path.join('js', `[id].${name}.[name].[chunkhash:3].js`),
+      globalObject: 'self',
+    },
+    // RESOLVE
+    resolve: {
+      modules: [path.resolve('.', 'src'), 'node_modules'],
+      extensions: ['.js', '.json', '.worker.js'],
+    },
+    // MODULE
+    module: {
+      rules: [
+        {
+          test: /\.worker\.js$/i,
+          use: [
+            {
+              loader: 'worker-loader',
+              options: {
+                name: path.join(
+                  'js',
+                  'workers',
+                  `[folder].${name}.[name].[hash:3].worker.js`
+                ),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/i,
+          include: [
+            path.resolve('src'),
+            path.resolve('node_modules', 'lodash-es'),
+            // path.resolve('node_modules', 'color-hash'),
+            path.resolve('node_modules', 'timing-functions'),
+            /protvista/i,
+            path.resolve('node_modules', 'd3'),
+            path.resolve('node_modules', 'data-loader'),
+            path.resolve('node_modules', 'interpro-components'),
+            path.resolve('node_modules', 'lit-html'),
+            path.resolve('node_modules', 'pdb-web-components'),
+          ],
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/env',
+                    {
+                      modules: false,
+                      loose: true,
+                      useBuiltIns: 'usage',
+                      targets: module
+                        ? { esmodules: true }
+                        : { browsers: '> 0.25%' },
+                    },
+                  ],
+                  ['@babel/react', { development: mode === 'development' }],
+                ],
+                plugins: [
+                  '@babel/plugin-syntax-dynamic-import',
+                  ['@babel/plugin-proposal-class-properties', { loose: true }],
+                ],
+                env: {
+                  dev: {
+                    // better sourcemaps for JSX code
+                    plugins: ['transform-react-jsx-source'],
+                  },
+                  production: {
+                    plugins: [
+                      // optimisations for react
+                      'babel-plugin-transform-react-remove-prop-types',
+                      'babel-plugin-transform-react-constant-elements',
+                      'babel-plugin-transform-react-inline-elements',
+                    ],
+                  },
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(txt|fast[aq])/i,
+          use: [
+            {
+              loader: 'raw-loader',
+            },
+          ],
+        },
+        {
+          test: /\.yml$/i,
+          use: [
+            {
+              loader: 'json-loader',
+            },
+            {
+              loader: 'yaml-loader',
+            },
+          ],
+        },
+        {
+          test: /((LiteMol-plugin-blue)|(LiteMol-plugin-light)|(LiteMol-plugin)|(tippy)|(clanviewer)|(ebi-global)|(interpro-new))\.css$/i,
+          use: [
+            {
+              loader:
+                mode === 'production'
+                  ? MiniCssExtractPlugin.loader
+                  : 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: Object.assign({}, cssSettings(mode), {
+                localIdentName: '[local]',
+              }),
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [postCSSImport, cssNext()],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader:
+                mode === 'production'
+                  ? MiniCssExtractPlugin.loader
+                  : 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: cssSettings(mode),
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [postCSSImport, cssNext()],
+              },
+            },
+          ],
+          exclude: /((LiteMol-plugin-blue)|(LiteMol-plugin-light)|(LiteMol-plugin)|(tippy)|(clanviewer)|(ebi-global)|(interpro-new))\.css$/i,
+        },
+        {
+          test: /\.scss$/i,
+          use: [
+            {
+              loader:
+                mode === 'production'
+                  ? MiniCssExtractPlugin.loader
+                  : 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: Object.assign({}, cssSettings(mode), {
+                localIdentName: '[local]',
+              }),
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                name: path.join(
+                  'assets',
+                  'images',
+                  '[name].[hash:base62:3].[ext]'
+                ),
+                limit: 1 * kB,
+              },
+            },
+            {
+              loader: 'img-loader',
+              options: {
+                enabled: mode === 'production',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                name: path.join(
+                  'assets',
+                  'fonts',
+                  '[name].[hash:base62:3].[ext]'
+                ),
+                limit: 1 * kB,
+                mimetype: 'application/font-woff',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: path.join(
+                  'assets',
+                  'fonts',
+                  '[name].[hash:base62:3].[ext]'
+                ),
+              },
+            },
+          ],
+        },
+      ],
+    },
+    // PLUGINS
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          STAGING: JSON.stringify(!!env.staging),
+        },
+        'process.info': JSON.stringify({
+          git: buildInfo.git,
+          build: buildInfo.build,
+        }),
+      }),
+      mode === 'production' ? miniCssExtractPlugin : null,
+      mode === 'production'
+        ? new (require('offline-plugin'))({
+            caches: {
+              main: [
+                new RegExp(`js/[^/]*${name}[^/]*.js$`, 'i'),
+                new RegExp(`css/[^/]+.css$`, 'i'),
+              ],
+              additional: [/\.(worker\.js)$/i],
+              optional: [/\.(eot|ttf|woff|svg|ico|png|jpe?g)$/i],
+            },
+            // TODO: check a way to use it without affecting /api
+            // appShell: publicPath,
+            AppCache: false,
+            // TODO: Check whats the best way to do this autoupdate.
+            // autoUpdate: 60000,
+            ServiceWorker: {
+              output: `sw.${name}.js`,
+              events: true,
+            },
+            safeToUseOptionalCaches: true,
+            excludes: ['**/.*', '**/*.{map,br,gz}'],
+          })
+        : null,
+      // Custom plugin to split codebase into legacy/modern bundles,
+      // depends on HTMLWebpackPlugin
+      legacyModuleSplitPlugin,
+      // GZIP compression
+      mode === 'production'
+        ? new (getCompressionPlugin())({
+            asset: '[path].gz[query]',
+            test: /\.(js|css|html|svg)$/i,
+            cache: true,
+            algorithm(buffer, options, callback) {
+              require('node-zopfli-es').gzip(buffer, options, callback);
+            },
+          })
+        : null,
+      // Brotli compression
+      mode === 'production'
+        ? new (getCompressionPlugin())({
+            asset: '[path].br[query]',
+            test: /\.(js|css|html|svg)$/i,
+            cache: true,
+            algorithm(buffer, _, callback) {
+              require('iltorb').compress(
+                buffer,
+                {
+                  mode: 1, // text
+                  quality: 11, // goes from 1 (but quick) to 11 (but slow)
+                },
+                callback
+              );
+            },
+          })
+        : null,
+    ].filter(Boolean),
+    // optimization.minimize: mode === 'production',
+    devtool: ((mode, env) => {
+      if (mode === 'development') return 'cheap-module-source-map';
+      if (env.staging) return 'source-map';
+      return false;
+    })(mode, env),
+  };
+};
 
 module.exports = (env = { dev: true }, { mode = 'production' }) => {
   const configModule = getConfigFor(env, mode, true);
