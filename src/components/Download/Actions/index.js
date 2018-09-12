b61cabcebe1036631090e13dced74286deb86805
pesseat <Rupture26>
2018-09-12 16:58:12 +0100

    update submit ticket icon to latest version changing the data-icon

--- src/components/Download/Actions/index.js
+++ src/components/Download/Actions/index.js
@@ -40,18 +40,22 @@ class Actions extends PureComponent {
             disabled={!blobURL}
             aria-label="Download job"
             data-icon="&#x3d;"
-            style={{ color: 'white' }}
+            style={{ fontSize: '160%', border: 'none', color: 'white' }}
           />
         </Tooltip>
         <Tooltip title="Delete job">
           <button
-            className={f('button', 'alert')}
-            type="button"
+            style={{ fontSize: '120%' }}
+            className={f(
+              'icon',
+              'icon-common',
+              'ico-neutral',
+              'margin-left-large',
+            )}
             onClick={this._handleDelete}
+            data-icon="&#xf1f8;"
             aria-label="Delete job"
-          >
-            ✖
-          </button>
+          />
         </Tooltip>
       </div>
     );
