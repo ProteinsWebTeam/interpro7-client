1a47d22d5e19b46d3e5a5933ab5d3b59ed54c1d8
Aurélien Luciani <luciani@ebi.ac.uk>
2018-09-14 15:25:59 +0100

    always take server settings from config for production

--- src/store/utils/get-initial-state/index.js
+++ src/store/utils/get-initial-state/index.js
@@ -1,9 +1,12 @@
 import { parse } from 'url';
+import config, { PROD } from 'config';
 
 import pathToDescription from 'utils/processDescription/pathToDescription';
 import parseParamToNumber from 'store/utils/parse-param-to-number';
 
 import settingsStorage from 'storage/settings';
+
+const DEFAULT_HTTP_PORT = 80;
 
 export default history => {
   const {
@@ -12,6 +15,26 @@ export default history => {
   let settings;
   if (settingsStorage) {
     settings = settingsStorage.getValue() || undefined;
+    if (PROD) {
+      settings.ebi = {
+        protocol: config.root.EBIsearch.protocol,
+        hostname: config.root.EBIsearch.hostname,
+        port: config.root.EBIsearch.port || DEFAULT_HTTP_PORT,
+        root: config.root.EBIsearch.pathname,
+      };
+      settings.api = {
+        protocol: config.root.API.protocol,
+        hostname: config.root.API.hostname,
+        port: config.root.API.port || DEFAULT_HTTP_PORT,
+        root: config.root.API.pathname,
+      };
+      settings.ipScan = {
+        protocol: config.root.IPScan.protocol,
+        hostname: config.root.IPScan.hostname,
+        port: config.root.IPScan.port || DEFAULT_HTTP_PORT,
+        root: config.root.IPScan.pathname,
+      };
+    }
   }
   const description = pathToDescription(pathname);
   return {
