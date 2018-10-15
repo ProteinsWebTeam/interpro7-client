983cf6e52c339e9518d8b7d09cf2710b9b0be6c6
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-10-15 11:01:03 +0100

    fixing bug of refreshing on an entry page

--- src/pages/endpoint-page.js
+++ src/pages/endpoint-page.js
@@ -50,7 +50,7 @@ const propTypes = {
     payload: T.object,
     loading: T.bool.isRequired,
     ok: T.bool,
-  }),
+  }).isRequired,
   subPagesForEndpoint: T.oneOfType([T.func, T.object]),
 };
 
@@ -69,14 +69,16 @@ class SummaryComponent extends PureComponent {
   render() {
     const {
       data: { payload, loading },
-      dataBase: {
-        payload: { databases },
-        loading: loadingDB,
-      },
+      dataBase: { payload: payloadDB, loading: loadingDB },
       customLocation,
       SummaryAsync,
     } = this.props;
-    const db = databases[customLocation.description.entry.db];
+    const db =
+      (!loadingDB &&
+        payloadDB &&
+        payloadDB.databases &&
+        payloadDB.databases[customLocation.description.entry.db]) ||
+      {};
     return (
       <SummaryAsync
         data={payload}
