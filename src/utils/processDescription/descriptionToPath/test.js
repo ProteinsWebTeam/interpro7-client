a430504f102674e8860c8a610e35b888e581a52b
CodeGit <matloob@gmail.com>
2018-10-31 14:23:39 +0000

    fixed test to handle ensure accessions are rendered in lowercase

--- src/utils/processDescription/descriptionToPath/test.js
+++ src/utils/processDescription/descriptionToPath/test.js
@@ -37,8 +37,9 @@ describe('descriptionToPath()', () => {
     test('with value', () => {
       for (const type of ['text', 'sequence']) {
         for (const value of ['kinase', 'ATCTATHDJKLWKLKWLDKAEE', 'GO:12345']) {
+          const lcValue = value.toLowerCase();
           expect(descriptionToPath({ ...d, search: { type, value } })).toBe(
-            `/search/${type}/${value}/`,
+            `/search/${type}/${lcValue}/`,
           );
         }
       }
