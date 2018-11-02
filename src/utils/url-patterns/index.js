13f3d94b2b9014a6bbbd646fbe7c44371f992690
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-11-02 12:03:50 +0000

    some eslint fix

--- src/utils/url-patterns/index.js
+++ src/utils/url-patterns/index.js
@@ -15,56 +15,35 @@ export const iproscan2urlDB = (db /*: string */) =>
   ]).get(db) || db.toLowerCase();
 
 export default (db /*: string */) => {
-  let pattern = '';
-  switch (db.toLowerCase()) {
-    case 'cathgene3d':
-      pattern = 'http://www.cathdb.info/version/latest/superfamily/{}';
-      break;
-    case 'cdd':
-      pattern = '//www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid={}';
-      break;
-    case 'hamap':
-      pattern = '//hamap.expasy.org/signature/{}';
-      break;
-    case 'panther':
-      pattern = 'http://www.pantherdb.org/panther/family.do?clsAccession={}';
-      break;
-    case 'pfam':
-      pattern = '//pfam.xfam.org/family/{}';
-      break;
-    case 'pirsf':
-      pattern = '//pir.georgetown.edu/cgi-bin/ipcSF?id={}';
-      break;
-    case 'prints':
-      pattern =
-        'http://www.bioinf.manchester.ac.uk/cgi-bin/dbbrowser/sprint/searchprintss.cgi?prints_accn={}&display_opts=Prints&category=None&queryform=false&regexpr=off';
-      break;
-    case 'prodom':
-      pattern =
-        'http://prodom.prabi.fr/prodom/current/cgi-bin/request.pl?question=DBEN&query={}';
-      break;
-    case 'profile':
-      pattern = '//prosite.expasy.org/{}';
-      break;
-    case 'prosite':
-      pattern = '//prosite.expasy.org/{}';
-      break;
-    case 'sfld':
-      pattern = 'http://sfld.rbvi.ucsf.edu/django/family/{}/';
-      break;
-    case 'smart':
-      pattern =
-        'http://smart.embl-heidelberg.de/smart/do_annotation.pl?DOMAIN={}';
-      break;
-    case 'ssf':
-      pattern = 'http://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid={}';
-      break;
-    case 'tigrfams':
-      pattern = 'http://www.jcvi.org/cgi-bin/tigrfams/HmmReportPage.cgi?acc={}';
-      break;
-    default:
-      pattern = '';
-  }
+  const patterns = new Map([
+    ['cathgene3d', 'http://www.cathdb.info/version/latest/superfamily/{}'],
+    ['cdd', '//www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid={}'],
+    ['hamap', '//hamap.expasy.org/signature/{}'],
+    ['panther', 'http://www.pantherdb.org/panther/family.do?clsAccession={}'],
+    ['pfam', '//pfam.xfam.org/family/{}'],
+    ['pirsf', '//pir.georgetown.edu/cgi-bin/ipcSF?id={}'],
+    [
+      'prints',
+      'http://www.bioinf.manchester.ac.uk/cgi-bin/dbbrowser/sprint/searchprintss.cgi?prints_accn={}&display_opts=Prints&category=None&queryform=false&regexpr=off',
+    ],
+    [
+      'prodom',
+      'http://prodom.prabi.fr/prodom/current/cgi-bin/request.pl?question=DBEN&query={}',
+    ],
+    ['profile', '//prosite.expasy.org/{}'],
+    ['prosite', '//prosite.expasy.org/{}'],
+    ['sfld', 'http://sfld.rbvi.ucsf.edu/django/family/{}/'],
+    [
+      'smart',
+      'http://smart.embl-heidelberg.de/smart/do_annotation.pl?DOMAIN={}',
+    ],
+    ['ssf', 'http://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid={}'],
+    [
+      'tigrfams',
+      'http://www.jcvi.org/cgi-bin/tigrfams/HmmReportPage.cgi?acc={}',
+    ],
+  ]);
+  const pattern = patterns.get(db.toLowerCase()) || '';
   if (!pattern) return;
   return (accession /*: string */) =>
     pattern.replace(
