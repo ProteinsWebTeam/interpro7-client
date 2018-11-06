6061fd7fec9b83a0727900ae9debf7bc89b8563f
pesseat <Rupture26>
2018-11-06 11:00:51 +0000

    replace classname "card-flex-container" by "grid-card"

--- src/components/home/ByLatestEntries/index.js
+++ src/components/home/ByLatestEntries/index.js
@@ -49,7 +49,7 @@ class LatestEntry extends PureComponent {
   render() {
     const { entry } = this.props;
     return (
-      <div className={f('card-flex-container', 'card-shrink')}>
+      <div className={f('grid-card', 'card-shrink')}>
         <div className={f('list-body')}>
           <div className={f('card-header')}>
             <Link
