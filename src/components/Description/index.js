5f7e6df4e84c5e0a4c51b2a6567312f8d04ddf8c
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-11-23 11:31:13 +0000

    protein function in description

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -12,8 +12,9 @@ import Link from 'components/generic/Link';
 import ebiStyles from 'ebi-framework/css/ebi-global.css';
 import styles from './style.css';
 import theme from 'styles/theme-interpro.css';
+import fonts from 'EBI-Icon-fonts/fonts.css';
 
-const f = foundationPartial(ebiStyles, styles, theme);
+const f = foundationPartial(ebiStyles, styles, theme, fonts);
 
 export const ParagraphWithCites = ({
   p,
@@ -206,6 +207,59 @@ class Description extends PureComponent /*:: <Props> */ {
           />
         ))}
       </div>
+    );
+  }
+}
+
+export class DescriptionReadMore extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { showMore: false };
+  }
+  render() {
+    const {
+      text = '',
+      minNumberOfCharToShow = Infinity,
+      patternToRemove,
+    } = this.props;
+    const { showMore } = this.state;
+    let textToShow = text;
+    if (patternToRemove) {
+      textToShow = textToShow.replace(new RegExp(patternToRemove, 'g'), '');
+    }
+    textToShow =
+      showMore || minNumberOfCharToShow > textToShow.length
+        ? textToShow
+        : `${textToShow.slice(0, minNumberOfCharToShow)}...`;
+    return (
+      <>
+        <Description textBlocks={[textToShow]} {...this.props} />
+        {minNumberOfCharToShow < text.length && (
+          <button
+            className={f('button', 'hollow', 'secondary', 'margin-bottom-none')}
+            onClick={() => this.setState({ showMore: !showMore })}
+          >
+            Show{' '}
+            {showMore ? (
+              <span>
+                Less{' '}
+                <i
+                  className={f('icon', 'icon-common', 'font-sm')}
+                  data-icon="&#xf102;"
+                />
+              </span>
+            ) : (
+              <span>
+                More{' '}
+                <i
+                  className={f('icon', 'icon-common', 'font-sm')}
+                  data-icon="&#xf103;"
+                />
+              </span>
+            )}
+          </button>
+        )}
+      </>
     );
   }
 }
