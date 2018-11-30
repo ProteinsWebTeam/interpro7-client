953a538e490137bd51043c8472bdaf3899789fe0
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-11-30 16:32:15 +0000

    using protein_structure_mapping to map features in structure

---
+++ src/components/Structure/Viewer/proteinToStructureMapper.js
@@ -0,0 +1,19 @@
+import { scaleLinear } from 'd3';
+
+export default mapCoordinates => {
+  const protein2structure = scaleLinear();
+  const coord = mapCoordinates
+    .sort(({ protein_start: a }, { protein_start: b }) => a - b)
+    .reduce(
+      (agg, v) => {
+        agg.domain.push(v.protein_start);
+        agg.domain.push(v.protein_end);
+        agg.range.push(v.structure_start);
+        agg.range.push(v.structure_end);
+        return agg;
+      },
+      { domain: [], range: [] },
+    );
+  protein2structure.domain(coord.domain).range(coord.range);
+  return protein2structure;
+};
