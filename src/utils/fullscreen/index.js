9683b780bbec3c28d9ef724e8f8124b8d428f6fd
CodeGit <maq@ebi.ac.uk>
2019-01-29 10:36:59 +0000

    Split screen toggle working properly. Still need error handling.

--- src/utils/fullscreen/index.js
+++ src/utils/fullscreen/index.js
@@ -1,4 +1,5 @@
 export const requestFullScreen = element => {
+  console.log(`requestFullScreen`);
   if ('requestFullscreen' in element) {
     element.requestFullscreen();
   }
@@ -10,5 +11,43 @@ export const requestFullScreen = element => {
   }
   if ('msRequestFullscreen' in element) {
     element.msRequestFullscreen();
+  }
+};
+
+export const exitFullScreen = element => {
+  console.log(`exitFullScreen`);
+  if ('exitFullscreen' in document) {
+    document.exitFullscreen();
+  }
+  if ('webkitExitFullscreen' in document) {
+    document.webkitExitFullscreen();
+  }
+  if ('mozExitFullScreen' in document) {
+    document.mozExitFullScreen();
+  }
+  if ('msExitFullscreen' in document) {
+    document.msExitFullscreen();
+  }
+};
+
+export const onFullScreenChange = (element, callback) => {
+  console.log(`onFullScreenChange`);
+  if ('onfullscreenchange' in element) {
+    element.onfullscreenchange = callback;
+  }
+  if ('onwebkitfullscreenchange' in element) {
+    element.onwebkitfullscreenchange = callback;
+  }
+  if ('onmozfullscreenchange' in element) {
+    element.onmozfullscreenchange = callback;
+  }
+  if ('MSwebkitfullscreenchange' in element) {
+    element.MSwebkitfullscreenchange = element;
+  }
+  if ('onmozfullscreenchange' in document) {
+    document.onmozfullscreenchange = callback;
+  }
+  if ('MSwebkitfullscreenchange' in document) {
+    document.MSwebkitfullscreenchange = callback;
   }
 };
