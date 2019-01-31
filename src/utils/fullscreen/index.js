08b740e07b31e8d8d7d852c3d4401b0b10beef8c
CodeGit <matloob@gmail.com>
2019-01-31 14:19:37 +0000

    updated after checking with docs and testing on firefox and safari

--- src/utils/fullscreen/index.js
+++ src/utils/fullscreen/index.js
@@ -2,14 +2,11 @@ export const requestFullScreen = element => {
   console.log(`requestFullScreen`);
   if ('requestFullscreen' in element) {
     element.requestFullscreen();
-  }
-  if ('webkitRequestFullscreen' in element) {
+  } else if ('webkitRequestFullscreen' in element) {
     element.webkitRequestFullscreen();
-  }
-  if ('mozRequestFullScreen' in element) {
+  } else if ('mozRequestFullScreen' in element) {
     element.mozRequestFullScreen();
-  }
-  if ('msRequestFullscreen' in element) {
+  } else if ('msRequestFullscreen' in element) {
     element.msRequestFullscreen();
   }
 };
@@ -18,14 +15,11 @@ export const exitFullScreen = element => {
   console.log(`exitFullScreen`);
   if ('exitFullscreen' in document) {
     document.exitFullscreen();
-  }
-  if ('webkitExitFullscreen' in document) {
+  } else if ('webkitExitFullscreen' in document) {
     document.webkitExitFullscreen();
-  }
-  if ('mozExitFullScreen' in document) {
+  } else if ('mozExitFullScreen' in document) {
     document.mozExitFullScreen();
-  }
-  if ('msExitFullscreen' in document) {
+  } else if ('msExitFullscreen' in document) {
     document.msExitFullscreen();
   }
 };
@@ -34,20 +28,17 @@ export const onFullScreenChange = (element, callback) => {
   console.log(`onFullScreenChange`);
   if ('onfullscreenchange' in element) {
     element.onfullscreenchange = callback;
-  }
-  if ('onwebkitfullscreenchange' in element) {
+  } else if ('onwebkitfullscreenchange' in element) {
     element.onwebkitfullscreenchange = callback;
-  }
-  if ('onmozfullscreenchange' in element) {
+  } else if ('onmozfullscreenchange' in element) {
     element.onmozfullscreenchange = callback;
-  }
-  if ('MSwebkitfullscreenchange' in element) {
-    element.MSwebkitfullscreenchange = element;
-  }
-  if ('onmozfullscreenchange' in document) {
+  } else if ('onfullscreenchange' in document) {
+    document.onfullscreenchange = callback;
+  } else if ('onwebkitfullscreenchange' in document) {
+    document.onwebkitfullscreenchange = callback;
+  } else if ('onmozfullscreenchange' in document) {
     document.onmozfullscreenchange = callback;
-  }
-  if ('MSwebkitfullscreenchange' in document) {
-    document.MSwebkitfullscreenchange = callback;
+  } else if ('onMSFullscreenChange' in document) {
+    document.onMSFullscreenChange = callback;
   }
 };
