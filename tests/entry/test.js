8353c361b09fbb77f7a283dda1ec784908f78b5b
CodeGit <maq@ebi.ac.uk>
2019-04-29 14:24:45 +0100

    added some tests for the title portion of the interpro entry pages

--- tests/entry/test.js
+++ tests/entry/test.js
@@ -9,6 +9,18 @@ describe('tests', () => {
   let page;
   let homepageURL;
 
+  const expectedElements = [
+    '[data-testid="entry-titlebar"]',
+    '[data-testid="entry-titlebar"] [data-testid="entry-type-icon"]',
+    '[data-testid="entry-titlebar"] [data-testid="entry-accession"]',
+    '[data-testid="entry-titlebar"] [data-testid="entry-title"]',
+    '[data-testid="entry-member-db-icon"]',
+  ];
+
+  const notExpectedElements = [
+    '[data-testid="entry-titlebar"] [data-testid="entry-member-db-icon"]',
+  ];
+
   beforeAll(async () => {
     page = await testSetup.setup();
     homepageURL = await page.evaluate(() => window.location.href);
@@ -16,20 +28,7 @@ describe('tests', () => {
 
   afterAll(testSetup.cleanup);
 
-  const pageElementTests = async (
-    clickFilter,
-    expectedElements,
-    notExpectedElements
-  ) => {
-    await Promise.all([
-      page.waitForSelector(`[data-testid="memberdb-filter-${clickFilter}"]`, {
-        timeout: 0,
-      }),
-      page.click(`[data-testid="memberdb-filter-${clickFilter}"]`, {
-        waitUntil: 'networkidle0',
-      }),
-    ]);
-
+  const pageElementTests = async (expectedElements, notExpectedElements) => {
     for (const selector of expectedElements) {
       const selection = await page.waitForSelector(selector);
       expect(selection).not.toBeNull();
@@ -56,5 +55,38 @@ describe('tests', () => {
     expect(url).toEqual(
       expect.stringMatching(/interpro\/entry\/interpro\/IPR/i)
     );
+  });
+
+  test('click-browse-entry-grid-item', async () => {
+    // initial navigation to browse page
+    const browseURL = `${homepageURL}entry/interpro/#grid`;
+    await Promise.all([page.waitForNavigation(), page.goto(browseURL)]);
+
+    // click first entry row
+    await Promise.all([
+      page.waitForSelector('[data-testid="grid-entity"] a', {
+        timeout: 0,
+      }),
+      page.click('[data-testid="grid-entity"] a'),
+    ]);
+    const url = await page.evaluate(() => window.location.href);
+    expect(url).toEqual(
+      expect.stringMatching(/interpro\/entry\/interpro\/IPR/i)
+    );
+  });
+
+  test('click-browse-entry-page-elements', async () => {
+    // initial navigation to browse page
+    const browseURL = `${homepageURL}entry/interpro/#table`;
+    await Promise.all([page.waitForNavigation(), page.goto(browseURL)]);
+
+    // click first entry row
+    await Promise.all([
+      page.waitForSelector('[data-testid="table-entity"] a', {
+        timeout: 0,
+      }),
+      page.click('[data-testid="table-entity"] a'),
+    ]);
+    await pageElementTests(expectedElements, notExpectedElements);
   });
 });
