e58d6907f44f8a083946256cff258b75468c669d
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2019-06-18 16:56:24 +0100

    Dynamic Tooltip with hooks

--- src/components/SimpleCommonComponents/DynamicTooltip/index.js
+++ src/components/SimpleCommonComponents/DynamicTooltip/index.js
@@ -1,60 +1,38 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
 import T from 'prop-types';
-// import Tippy from '@tippy.js/react';
 import loadData from 'higherOrder/loadData';
-import Loading from 'components/SimpleCommonComponents/Loading';
 import Tooltip from 'components/SimpleCommonComponents/Tooltip';
 
 import descriptionToPath from 'utils/processDescription/descriptionToPath';
-import { getUrlForMeta } from '../../../higherOrder/loadData/defaults';
 import { createSelector } from 'reselect';
 import { format } from 'url';
 
-class _DynamicTooltip extends PureComponent /*:: <Pros> */ {
-  static propTypes = {
-    data: T.shape({
-      payload: T.object,
-      loading: T.bool,
-    }).isRequired,
-    dataDB: T.shape({
-      metadata: T.object,
-      loading: T.bool,
-    }).isRequired,
-    type: T.string.isRequired,
-    source: T.string.isRequired,
-    accession: T.string.isRequired,
-  };
-
-  render() {
+const _DataProvider = ({ data, onLoad }) => {
+  if (data && !data.loading && data.payload && data.payload.metadata) {
     const {
-      type,
-      source,
       accession,
-      data,
-      dataDB,
-      isStaleDB: _isStaleDB,
-      isStale: _isStale,
-      ...rest
-    } = this.props;
+      name: { name },
+      source_database: db,
+    } = data.payload.metadata;
 
-    let message = '';
-    if (!dataDB || !data || dataDB.loading || data.loading) {
-      message += `<b>${accession} </b></br>`;
-    } else {
-      const name = data.payload.metadata.name.name;
-      const displaySourceDB = dataDB.payload.databases[source].name;
-      const dataType = data.payload.metadata.type;
-      message += `<b>${accession}</b></br>`;
-      message += `${name} </br>`;
-      message += `${displaySourceDB}`;
-      if (dataType) message += ` ${dataType}`;
-    }
+    const message = `
+        <b>${accession}</b></br>
+        ${name} </br>
+        ${db} </br>
+    `;
 
-    return <Tooltip html={message} {...rest} />;
+    onLoad(message);
   }
-}
-
+  return null;
+};
+_DataProvider.propTypes = {
+  data: T.shape({
+    payload: T.object,
+    loading: T.bool,
+  }),
+  onLoad: T.func.isRequired,
+};
 const getUrlFor = createSelector(
   state => state.settings.api,
   (_, props) => props,
@@ -72,12 +50,37 @@ const getUrlFor = createSelector(
   },
 );
 
-export const DynamicTooltip = _DynamicTooltip;
-export default loadData({
-  getUrl: getUrlForMeta,
-  propNamespace: 'DB',
-})(
-  loadData({
-    getUrl: getUrlFor,
-  })(DynamicTooltip),
-);
+const DynamicTooltip = ({ type, source, accession, ...props }) => {
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const [message, setMessage] = useState(accession);
+  const _handleMouseOver = () => {
+    setShouldLoad(true);
+  };
+  const DataProvider = shouldLoad
+    ? loadData(getUrlFor)(_DataProvider)
+    : _DataProvider;
+
+  return (
+    <>
+      <DataProvider
+        accession={accession}
+        type={type}
+        source={source}
+        onLoad={setMessage}
+      />
+      <Tooltip
+        html={message}
+        {...props}
+        onMouseOver={_handleMouseOver}
+        onFocus={_handleMouseOver}
+      />
+    </>
+  );
+};
+DynamicTooltip.propTypes = {
+  type: T.string.isRequired,
+  source: T.string.isRequired,
+  accession: T.string.isRequired,
+};
+
+export default DynamicTooltip;
