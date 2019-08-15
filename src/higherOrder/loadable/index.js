a51ba4ee77a6f782cff7d778bba8bf7d1ee9f350
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2019-08-15 17:17:45 +0100

    fixing loadable to only render when there are changes

--- src/higherOrder/loadable/index.js
+++ src/higherOrder/loadable/index.js
@@ -7,21 +7,17 @@ const DEFAULT_TIMEOUT = 15000;
 
 // TODO: find better flow types
 // TODO: Updating to flow ^0.85 makes this mandatory and reports many errors for connect()
-const loadable = ({ loader, loading }) /*: Object */ => props => {
+const loadable = ({ loader, loading }) /*: Object */ => {
   if (!loader) return;
   const Loaded = React.lazy(loader);
   const Loading = loading || LoadingComponent;
-  return (
-    <React.Suspense fallback={<Loading />}>
-      <Loaded {...props} />
-    </React.Suspense>
-  );
+  return React.memo(props => {
+    return (
+      <React.Suspense fallback={<Loading />}>
+        <Loaded {...props} />
+      </React.Suspense>
+    );
+  });
 };
-// reactLoadable({
-//   loading: LoadingComponent,
-//   delay: DEFAULT_DELAY,
-//   timeout: DEFAULT_TIMEOUT,
-//   ...options,
-// });
 
 export default loadable;
