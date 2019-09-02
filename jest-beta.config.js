f2df2f29d9588f909528d8cfd7c172f1e79a4e7d
CodeGit <maq@ebi.ac.uk>
2019-09-02 14:45:34 +0100

    setup up jest config files to tests to run against dev or beta server

--- jest-beta.config.js
+++ jest-beta.config.js
@@ -1,22 +1,7 @@
 /* eslint-env node */
 
+const { defaults } = require('jest-config');
+
 module.exports = {
-  verbose: false,
-  moduleNameMapper: {
-    'config.yml$': '<rootDir>/interpro-config.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|tmpl|fasta)$':
-      '<rootDir>/src/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-    '^taxonomy-visualisation(.*)$':
-      '<rootDir>/node_modules/taxonomy-visualisation/src/$1',
-    '^protvista-([^/]*)(.*)$': '<rootDir>/node_modules/protvista-$1/src/$2',
-  },
-  moduleDirectories: ['node_modules', 'src'],
-  collectCoverageFrom: ['src/**/*.js', '!**/.*.js'],
-  coverageReporters: ['json-summary', 'lcov', 'text-summary'],
-  coverageDirectory: 'reports/jest-coverage',
-  transformIgnorePatterns: [
-    'node_modules/(?!lodash-es|timing-functions|gsap|taxonomy-visualisation|protvista|react-syntax-highlighter)',
-  ],
   testURL: 'https://www.ebi.ac.uk/interpro/beta',
 };
