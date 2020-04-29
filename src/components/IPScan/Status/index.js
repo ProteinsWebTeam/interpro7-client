05103589048df858086336f5671e1286c202ba26
Swaathik <swaathik@gmail.com>
2020-04-29 10:06:13 +0100

    UX changes in InterProScan results page

--- src/components/IPScan/Status/index.js
+++ src/components/IPScan/Status/index.js
@@ -98,6 +98,12 @@ export class IPScanStatus extends PureComponent /*:: <Props> */ {
     return (
       <div className={f('row', 'columns')}>
         <h3 className={f('light')}>Your InterProScan Search Results</h3>
+        <p className={f('info')}>
+          Your InterProScan search results are listed below. Each search may run
+          for different time. You can navigate to other pages if you prefer.
+          Once the job is finished, you will be notified and the results will be
+          available for 7 days.
+        </p>
         <SchemaOrgData
           data={{
             name: 'Your InterProScan searches',
@@ -149,7 +155,7 @@ export class IPScanStatus extends PureComponent /*:: <Props> */ {
           >
             Results
           </Column>
-          <Column dataKey="localTitle">Title</Column>
+          <Column dataKey="localTitle">Name</Column>
           <Column
             dataKey="times"
             renderer={(
@@ -176,7 +182,7 @@ export class IPScanStatus extends PureComponent /*:: <Props> */ {
             dataKey="status"
             headerClassName={f('table-center')}
             cellClassName={f('table-center')}
-            renderer={(status /*: string */) => (
+            renderer={(status /*: string */, row /*: Object */) => (
               <Tooltip title={`Job ${status}`}>
                 {(status === 'running' ||
                   status === 'created' ||
@@ -199,12 +205,24 @@ export class IPScanStatus extends PureComponent /*:: <Props> */ {
                   />
                 ) : null}
                 {status === 'finished' && (
-                  <span
-                    style={{ fontSize: '160%' }}
-                    className={f('icon', 'icon-common', 'ico-confirmed')}
-                    data-icon="&#xf00c;"
-                    aria-label="Job finished"
-                  />
+                  <Link
+                    to={{
+                      description: {
+                        main: { key: 'result' },
+                        result: {
+                          type: 'InterProScan',
+                          accession: row.remoteID,
+                        },
+                      },
+                    }}
+                  >
+                    <span
+                      style={{ fontSize: '160%' }}
+                      className={f('icon', 'icon-common', 'ico-confirmed')}
+                      data-icon="&#xf00c;"
+                      aria-label="Job finished"
+                    />
+                  </Link>
                 )}
               </Tooltip>
             )}
@@ -226,12 +244,18 @@ export class IPScanStatus extends PureComponent /*:: <Props> */ {
   }
 }
 
+const compare = (a, b) => {
+  if (b.remoteID) return b.remoteID > a.remoteID ? 1 : -1;
+  return 1;
+};
+
 const mapsStateToProps = createSelector(
   (state) =>
     Object.values(state.jobs || {})
       .map((j) => j.metadata)
-      .sort((a, b) =>
-        (b.remoteID || b.localID) > (a.remoteID || a.localID) ? 1 : -1,
+      .sort(
+        compare,
+        // (b.remoteID || b.localID) > (a.remoteID || a.localID) ? 1 : -1,
       ),
   (state) => state.customLocation.search,
   (state) => state.settings.navigation.pageSize,
