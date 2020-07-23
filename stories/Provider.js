9895f1084de7d09d7e4d119cd11912c7130908c1
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2020-07-23 17:06:40 +0100

    fixing tests

--- stories/Provider.js
+++ stories/Provider.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import T from 'prop-types';
+
 import { Provider } from 'react-redux';
 
 const ProviderWrapper = ({ children, store }) => (
   <Provider store={store}>{children}</Provider>
 );
+ProviderWrapper.propTypes = {
+  children: T.any,
+  store: T.object,
+};
 
 export default ProviderWrapper;
