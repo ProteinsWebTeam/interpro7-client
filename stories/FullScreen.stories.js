3944a01f23eb7caa74c60f13bfea4ecc30c20ae4
Swaathik <swaathik@gmail.com>
2020-10-23 17:45:40 +0530

    Full screen button added to stories

--- stories/FullScreen.stories.js
+++ stories/FullScreen.stories.js
@@ -1,21 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import FullScreenButton from '../src/components/SimpleCommonComponents/FullScreenButton';
 
+import { foundationPartial } from '../src/styles/foundation';
+import fonts from 'EBI-Icon-fonts/fonts.css';
+const f = foundationPartial(fonts);
 export default {
   title: 'Basic UI/FullScreen',
 };
 
 export const Basic = () => {
-  const sampleRef = useRef();
+  const sampleRef = useRef(null);
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    setRendered(true);
+  }, [sampleRef]);
   return (
-    <>
-      <div id="sampleDiv" ref={sampleRef}>
-        Hello
-      </div>
-      <FullScreenButton
-        element={sampleRef}
-        tooltip="View it in full screen mode"
-      />
-    </>
+    <div ref={sampleRef}>
+      Content...
+      {rendered ? (
+        <FullScreenButton
+          element={sampleRef.current}
+          className={f('icon', 'icon-common')}
+          tooltip="View it in full screen mode"
+        />
+      ) : null}
+    </div>
   );
 };
