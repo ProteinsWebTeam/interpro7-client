725794efd137652aa099196ba7d8d1d77a815aab
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-02-09 17:29:25 +0000

    gh actions -lighthouse 2 - changing port just in case

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:8000/interpro'],
-      startServerCommand: 'mv dist interpro && http-server',
+      url: ['http://localhost:4000/interpro'],
+      startServerCommand: 'mv dist interpro && http-server -p 4000',
     },
     upload: {
       target: 'temporary-public-storage',
