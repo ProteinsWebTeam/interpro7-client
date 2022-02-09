6885b9e4d4c26f64c55d87f1c6c4cfd6fc9d5e65
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-02-09 17:12:18 +0000

    gh actions -lighthouse 1 - temporarily disabling tests and report

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:8000/interpro'],
-      startServerCommand: 'mv dist interpro && npx http-server',
+      startServerCommand: 'mv dist interpro && http-server',
     },
     upload: {
       target: 'temporary-public-storage',
