5780d245b64929b7232dbcb1d4b6cfef14034ea1
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-02-10 15:59:58 +0000

    removing custom script of lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:4000/interpro'],
+      url: ['http://localhost:4000/interpro/'],
       startServerCommand: 'mv dist interpro && http-server -p 4000',
     },
     upload: {
