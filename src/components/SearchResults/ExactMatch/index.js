c54de33ff6f368af5b3d761651b4fa8e499158dd
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2022-06-28 13:26:19 +0100

    exact search by gene

--- src/components/SearchResults/ExactMatch/index.js
+++ src/components/SearchResults/ExactMatch/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import T from 'prop-types';
 import { dataPropType } from 'higherOrder/loadData/dataPropTypes';
 
@@ -18,6 +18,12 @@ import ipro from 'styles/interpro-new.css';
 const f = foundationPartial(ebiGlobalStyles, fonts, ipro);
 
 const INTERPRO_ACCESSION_PADDING = 6;
+
+const ENTRY_NAME_REGEX = /^[a-zA-Z0-9]+(_[a-zA-Z0-9]+)?$/;
+
+const canBeUniprotEntryName = (term /*: string */) /*: boolean */ => {
+  return ENTRY_NAME_REGEX.test(term);
+};
 
 /*:: type EMWProps = {
   to: Object,
@@ -42,6 +48,53 @@ class ExactMatchWrapper extends PureComponent /*:: <EMWProps> */ {
     );
   }
 }
+
+/*:: type EMGWProps = {
+  matches: {
+    accession: string[],
+    name: string,
+  },
+  gene: string,
+}; */
+const ExactGeneMatchWrapper = ({ matches, gene }) => (
+  <div className={f('callout', 'warning', 'margin-bottom-medium')}>
+    <span className={f('icon', 'icon-common')} data-icon="&#xf35a;">
+      {' '}
+      Found an exact gene match for <b>{gene}</b>:
+    </span>{' '}
+    <ul>
+      {matches.map(({ accessions, name }) => (
+        <li key={name}>
+          <b>{name}</b>:{' '}
+          {accessions.map((accession, i) => (
+            <Fragment key={accession}>
+              {i > 0 ? ', ' : ''}
+              <Link
+                to={{
+                  description: {
+                    main: { key: 'protein' },
+                    protein: { db: 'uniprot', accession },
+                  },
+                }}
+              >
+                {accession}
+              </Link>
+            </Fragment>
+          ))}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+ExactGeneMatchWrapper.propTypes = {
+  matches: T.arrayOf(
+    T.shape({
+      accession: T.arrayOf(T.string),
+      name: T.string,
+    }),
+  ),
+  gene: T.string,
+};
 
 /*:: type SMProps = {
   data: {
@@ -75,38 +128,61 @@ export class ExactMatch extends PureComponent /*:: <SMProps> */ {
     );
     const exactMatches = new Map();
     if (payload) {
-      const { accession, endpoint: type, source_database: db } = payload;
-      if (searchRE.test(accession)) {
+      const {
+        accession,
+        endpoint: type,
+        source_database: db,
+        proteins,
+      } = payload;
+      if (typeof accession === 'string') {
+        if (searchRE.test(accession)) {
+          exactMatches.set(
+            type,
+            <ExactMatchWrapper
+              key={type}
+              to={{
+                description: {
+                  main: { key: type },
+                  [type]: { db, accession },
+                },
+              }}
+            >
+              {type} {accession}
+            </ExactMatchWrapper>,
+          );
+        }
+        // For identifier names (ex: VAV_HUMAN)
+        if (searchValue && searchValue.includes('_')) {
+          exactMatches.set(
+            type,
+            <ExactMatchWrapper
+              key={type}
+              to={{
+                description: {
+                  main: { key: type },
+                  [type]: { db, accession },
+                },
+              }}
+            >
+              {type} {accession} [ID: {searchValue.toUpperCase()}]
+            </ExactMatchWrapper>,
+          );
+        }
+      } else if (proteins?.length) {
+        // for search with genes, where one ID can have multiple proteins
+        const geneMatch = {};
+        for (const { accession: acc, organism, tax_id: taxon } of proteins) {
+          if (!geneMatch[taxon])
+            geneMatch[taxon] = { name: organism, accessions: [] };
+          geneMatch[taxon].accessions.push(acc);
+        }
         exactMatches.set(
-          type,
-          <ExactMatchWrapper
-            key={type}
-            to={{
-              description: {
-                main: { key: type },
-                [type]: { db, accession },
-              },
-            }}
-          >
-            {type} {accession}
-          </ExactMatchWrapper>,
-        );
-      }
-      // For identifier names (ex: VAV_HUMAN)
-      if (searchValue && searchValue.includes('_')) {
-        exactMatches.set(
-          type,
-          <ExactMatchWrapper
-            key={type}
-            to={{
-              description: {
-                main: { key: type },
-                [type]: { db, accession },
-              },
-            }}
-          >
-            {type} {accession} [ID: {searchValue.toUpperCase()}]
-          </ExactMatchWrapper>,
+          'gene-match',
+          <ExactGeneMatchWrapper
+            key="gene-match"
+            matches={Object.values(geneMatch)}
+            gene={searchValue.toUpperCase()}
+          />,
         );
       }
     }
@@ -154,7 +230,7 @@ const getSearchStringUrl = createSelector(
   ({ protocol, hostname, port, root }, searchValue) => {
     if (!searchValue) return null;
     const query = getQueryTerm(searchValue);
-    if (getURLByAccession(query)) {
+    if (getURLByAccession(query) || canBeUniprotEntryName(query)) {
       const param = `utils/accession/${query}`;
       return `${protocol}//${hostname}:${port}${root}${param}`;
     }
