3610fb6c00a723fcca306efc2406496f538f99b9
Matthias Blum <mat.blum@gmail.com>
2022-07-22 11:31:18 +0100

    Fix #399 by updating the source database

--- src/utils/processDescription/handlers/index.js
+++ src/utils/processDescription/handlers/index.js
@@ -537,11 +537,14 @@ export const setAccessionHandler /*: Handler */ = handlerConstructor({
     value: (value) => value,
   },
   match: {
-    value(current) {
+    value(current, description) {
       // $FlowFixMe object-this-reference
       const _current = this.cleanUp(current);
-      for (const { re } of setDBs) {
-        if (re.test(_current)) return true;
+      for (const { name, re } of setDBs) {
+        if (re.test(_current)) {
+          description.set.db = name;
+          return true;
+        }
       }
     },
   },
