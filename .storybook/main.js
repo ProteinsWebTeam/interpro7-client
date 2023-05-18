eb46a2bdaafb2f5d7e0f95728f868afa2bdadf75
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2023-05-18 17:37:15 +0100

    automigrate storybook, but diabling it from release

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 const path = require('path');
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../stories/**/*.stories.js'],
   addons: [
     '@storybook/addon-storysource',
@@ -12,23 +8,34 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-postcss',
   ],
-  typescript: { reactDocgen: 'none' },
+  typescript: {
+    reactDocgen: 'none',
+  },
   webpackFinal: async (config) => {
     // do mutation to the config
     config.resolve.modules.push(path.resolve('.', 'src'));
     const cssRule = config.module.rules.find(({ test }) =>
       test.toString().includes('.css')
     );
-
     cssRule.exclude = /((tippy)|(clanviewer))\.css$/i;
-
     config.module.rules.push({
       test: /((tippy)|(clanviewer))\.css$/i,
-      use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+      ],
     });
     config.module.rules.push({
       test: /\.yml$/i,
-      use: [{ loader: 'yaml-loader' }],
+      use: [
+        {
+          loader: 'yaml-loader',
+        },
+      ],
     });
     config.module.rules.push({
       test: /\.ts$/,
@@ -55,5 +62,9 @@ module.exports = {
     });
     // console.dir(config.module.rules);
     return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
 };
