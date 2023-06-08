c3e9ab1ed6f083bd7519ddd550e639a43f7e5c35
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2023-06-08 11:06:52 +0100

    starting a folder for hooks

---
+++ src/utils/hooks/useInterval.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type Fun = () => void;
+export const useInterval = (callback: Fun, delay: number) => {
+  const savedCallback = React.useRef<Fun>();
+  const [id, setId] = React.useState<NodeJS.Timer  |undefined>(undefined);
+
+  // Remember the latest callback.
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  const clear = ()=>{
+    if (id){
+    clearInterval(id);
+    setId(undefined);
+  }
+  }
+  // Set up the interval.
+  React.useEffect(() => {
+    const tick = () => savedCallback?.current?.();
+    if (delay !== null) {
+      setId( setInterval(tick, delay));
+      return () => {clear()}
+    }
+  }, [delay]);
+  return [clear];
+};
+
+export default useInterval;
