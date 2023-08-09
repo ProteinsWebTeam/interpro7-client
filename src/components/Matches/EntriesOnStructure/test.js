9bf117725a549370d83ca019ec607d2fc1bbd45b
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2023-08-09 17:23:30 +0100

    updating snapshots for matches

--- src/components/Matches/EntriesOnStructure/test.js
+++ src/components/Matches/EntriesOnStructure/test.js
@@ -11,70 +11,72 @@ describe('<EntriesOnStructure />', () => {
       <EntriesOnStructure
         matches={[
           {
-            entry: {
-              accession: 'IPR000001',
-              counters: {
-                domain_architectures: 12,
-                proteins: 19,
-                proteomes: 4,
-                sets: 0,
-                structures: 698,
-                taxa: 5,
+            entry_structure_locations: [
+              {
+                fragments: [
+                  {
+                    'dc-status': 'CONTINUOUS',
+                    end: 188,
+                    start: 105,
+                  },
+                ],
               },
-              cross_references: {},
-              description: ['<p>Kringle</p>'],
-              overlaps_with: [
-                {
-                  accession: 'IPR013806',
-                  name: 'Kringle-like fold',
-                  type: 'homologous_superfamily',
-                },
-                {
-                  accession: 'IPR038178',
-                  name: 'Kringle superfamily',
-                  type: 'homologous_superfamily',
-                },
-              ],
-              source_database: 'interpro',
-              type: 'domain',
-              name: {
-                name: 'Kringle',
-                short: 'Kringle',
+              {
+                fragments: [
+                  {
+                    'dc-status': 'CONTINUOUS',
+                    end: 293,
+                    start: 211,
+                  },
+                ],
               },
-            },
-            structure: {
-              accession: '1a4w',
-              chain: 'H',
-              entry_protein_locations: [
-                {
-                  fragments: [
-                    {
-                      'dc-status': 'CONTINUOUS',
-                      end: 188,
-                      start: 105,
-                    },
-                  ],
-                },
-                {
-                  fragments: [
-                    {
-                      'dc-status': 'CONTINUOUS',
-                      end: 293,
-                      start: 211,
-                    },
-                  ],
-                },
-              ],
-              experiment_type: 'x-ray',
-              name: "CRYSTAL STRUCTURES OF THROMBIN WITH THIAZOLE-CONTAINING INHIBITORS: PROBES OF THE S1' BINDING SITE",
-              protein: 'p00734',
-              protein_length: 622,
-              resolution: 1.8,
-              source_database: 'pdb',
-              structure_protein_locations: [],
-            },
+            ],
           },
         ]}
+        match={{
+          entry: {
+            accession: 'IPR000001',
+            counters: {
+              domain_architectures: 12,
+              proteins: 19,
+              proteomes: 4,
+              sets: 0,
+              structures: 698,
+              taxa: 5,
+            },
+            cross_references: {},
+            description: ['<p>Kringle</p>'],
+            overlaps_with: [
+              {
+                accession: 'IPR013806',
+                name: 'Kringle-like fold',
+                type: 'homologous_superfamily',
+              },
+              {
+                accession: 'IPR038178',
+                name: 'Kringle superfamily',
+                type: 'homologous_superfamily',
+              },
+            ],
+            source_database: 'interpro',
+            type: 'domain',
+            name: {
+              name: 'Kringle',
+              short: 'Kringle',
+            },
+          },
+          structure: {
+            accession: '1a4w',
+            chain: 'H',
+            experiment_type: 'x-ray',
+            name: "CRYSTAL STRUCTURES OF THROMBIN WITH THIAZOLE-CONTAINING INHIBITORS: PROBES OF THE S1' BINDING SITE",
+            protein: 'p00734',
+            protein_length: 622,
+            resolution: 1.8,
+            source_database: 'pdb',
+            structure_protein_locations: [],
+          },
+        }}
       />,
     );
     expect(renderer.getRenderOutput()).toMatchSnapshot();
