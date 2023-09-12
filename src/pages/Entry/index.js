b3fe2ca0cd19ebedbb64265b74757d43c91add02
Matthias Blum <mat.blum@gmail.com>
2023-09-12 12:58:31 +0100

    Info message for SFLD/PRINTS + link to RtD

--- src/pages/Entry/index.js
+++ src/pages/Entry/index.js
@@ -9,7 +9,6 @@ import {
 import Tooltip from 'components/SimpleCommonComponents/Tooltip';
 
 import Link from 'components/generic/Link';
-import ContentFromRTD from 'components/ContentFromRTD';
 // $FlowFixMe
 import GoLink from 'components/ExtLink/GoLink';
 import Description from 'components/Description';
@@ -416,6 +415,45 @@ class EntryCard extends PureComponent /*:: <EntryCardProps> */ {
   }
 }
 
+const ArchiveCallout = ({ name, page }) => (
+  <div className={f('row')}>
+    <div className={f('columns', 'large-12')}>
+      <div
+        className={f('callout', 'info')}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <span
+          style={{
+            fontSize: '2em',
+            color: 'darkblue',
+            paddingRight: '1rem',
+          }}
+          className={f('small', 'icon', 'icon-common', 'icon-info')}
+        />{' '}
+        <p>
+          <strong>{name} has retired</strong>
+          <br />
+          While {name} is no longer receiving updates, InterPro now serves as an
+          archival source, granting continued access to its data.
+          <br />
+          Further information about {name} can be found{' '}
+          <Link
+            href={config.root.readthedocs.href + page + '.html'}
+            className={f('ext')}
+            target="_blank"
+          >
+            in our documentation
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
 const AllEntriesDownload = (
   {
     description,
@@ -534,7 +572,7 @@ class List extends PureComponent /*:: <Props> */ {
             />
           )}
           {db && ['prints', 'sfld'].includes(db) ? (
-            <ContentFromRTD page={`${db}.rst`} />
+            <ArchiveCallout name={db.toUpperCase()} page={db.toLowerCase()} />
           ) : null}
           <Table
             dataTable={_payload.results}
