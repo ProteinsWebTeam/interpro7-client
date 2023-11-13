b6f11f6794377c906134236ee4a698fea70f4927
Matthias Blum <mat.blum@gmail.com>
2023-11-13 16:16:00 +0000

    update test and snapshot

--- src/components/Description/DescriptionLLM/test.js
+++ src/components/Description/DescriptionLLM/test.js
@@ -4,11 +4,14 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import DescriptionLLM from '.';
 
 const renderer = new ShallowRenderer();
+const accession = 'PTHR48251';
 const description = '<p>The function of the family is not clear.</p>';
 
 describe('<DescriptionLLM />', () => {
   test('should render', () => {
-    renderer.render(<DescriptionLLM text={description} />);
+    renderer.render(
+      <DescriptionLLM accession={accession} text={description} />,
+    );
     expect(renderer.getRenderOutput()).toMatchSnapshot();
   });
 });
