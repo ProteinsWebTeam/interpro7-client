8426fcc546ed34b09a32df993e48561a1f991ae7
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2024-02-19 14:46:22 +0000

    #FIX 575: when click on a family in clan viewer checks if the command key is pressed to open the link in a new teb

--- src/components/Set/ClanViewer/index.tsx
+++ src/components/Set/ClanViewer/index.tsx
@@ -141,15 +141,20 @@ class ClanViewer extends PureComponent<Props, State> {
       .filter((e) => (e as HTMLElement).nodeName === 'g')
       .filter((e) => (e as HTMLElement).classList.contains('node'))?.[0];
     if (g) {
-      this.props.goToCustomLocation({
-        description: {
-          main: { key: 'entry' },
-          entry: {
-            db: this.props.db,
-            accession: (g as HTMLElement).dataset.accession,
+      const accession = (g as HTMLElement).dataset.accession;
+      if ((event as MouseEvent).metaKey || (event as MouseEvent).altKey) {
+        window.open(`/entry/${this.props.db}/${accession}`, '_blank')?.focus();
+      } else {
+        this.props.goToCustomLocation({
+          description: {
+            main: { key: 'entry' },
+            entry: {
+              db: this.props.db,
+              accession,
+            },
           },
-        },
-      });
+        });
+      }
     }
   };
   _refreshLabels = () => {
