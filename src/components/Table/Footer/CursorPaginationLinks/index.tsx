e4d08bb6110ac0029f2394c702f355bf717e638b
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2024-03-12 17:05:32 +0000

    TS: Table/Footer

--- src/components/Table/Footer/CursorPaginationLinks/index.tsx
+++ src/components/Table/Footer/CursorPaginationLinks/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PaginationItem from '../PaginationItem';
 
-type ItemProps = { cursor: string; label: string };
+type ItemProps = { cursor?: string; label: string };
 const CursorPaginationItem = ({ cursor, label }: ItemProps) => (
   <PaginationItem value={cursor || '_'} noLink={!cursor} attributeName="cursor">
     {label}
   </PaginationItem>
 );
 
-type Props = { next: string; previous: string };
+type Props = {
+  next?: string;
+  previous: string;
+};
 const CursorPaginationLinks = ({ next, previous }: Props) => (
   <>
     <CursorPaginationItem cursor={previous} label="Previous" />
