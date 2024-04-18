cc772aa51497b6a887edae4dce360a6d0ec2becb
Matthias Blum <mat.blum@gmail.com>
2024-04-18 14:22:03 +0100

    Start migrating EBIHeader to TS

---
+++ src/components/EBIHeader/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'components/generic/Link';
+import cssBinder from 'styles/cssBinder';
+import local from './style.css';
+import ipro from 'styles/interpro-vf.css';
+const css = cssBinder(local, ipro);
+
+export const EBIHeader = () => (
+  <header
+    id="masthead-black-bar"
+    className={css('clearfix', 'masthead-black-bar', 'tmp-ebi-header')}
+  >
+    <div>
+      <nav className={css('row')}>
+        <ul id="global-nav" className={css('menu', 'global-nav', 'text-right')}>
+          <li className={css('ebi')}>
+            <Link href="//www.ebi.ac.uk">EMBL-EBI</Link>
+          </li>
+
+          <li className={css('services')}>
+            <Link href="//www.ebi.ac.uk/services">Services</Link>
+          </li>
+
+          <li className={css('research')}>
+            <Link href="//www.ebi.ac.uk/research">Research</Link>
+          </li>
+
+          <li className={css('training')}>
+            <Link href="//www.ebi.ac.uk/training">Training</Link>
+          </li>
+
+          <li className={css('about')}>
+            <Link href="//www.ebi.ac.uk/about">About us</Link>
+          </li>
+
+          <li
+            className={css(
+              'float-right',
+              'show-for-medium',
+              'embl-selector',
+              'embl-ebi',
+            )}
+          >
+            <button
+              className={css('button', 'float-right')}
+              type="button"
+              aria-expanded="false"
+              aria-label="EMBL dropdown"
+              style={{
+                outlineStyle: 'none',
+              }}
+            >
+              &nbsp;
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
+
+export default EBIHeader;
