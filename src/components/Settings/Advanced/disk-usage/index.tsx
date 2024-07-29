467adf29d60387c9dc5fe63b0733ee20799d9108
apolignano <alessandro.polignano@ebi.ac.uk>
2024-07-29 12:34:30 +0100

    Migrated DiskUsage component to TS

--- src/components/Settings/Advanced/disk-usage/index.tsx
+++ src/components/Settings/Advanced/disk-usage/index.tsx
@@ -1,7 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { filesize } from 'filesize';
 
-const getFileSize = (bytes) => filesize(bytes, { round: 0, standard: 'iec' });
+const getFileSize = (bytes: number) =>
+  filesize(bytes, { round: 0, standard: 'iec' });
 
 const support = 'storage' in navigator && 'estimate' in navigator.storage;
 
@@ -12,6 +13,51 @@ const getEstimate = () => {
 
 const PERCENT = 100;
 
+const DiskUsage = () => {
+  const [storageEstimate, setStorageEstimate] = useState<StorageEstimate>({
+    usage: NaN,
+    quota: NaN,
+  });
+
+  const fetchDiskData = useCallback(async () => {
+    const diskData: StorageEstimate = await getEstimate();
+    setStorageEstimate({ usage: diskData.usage, quota: diskData.quota });
+  }, []);
+
+  useEffect(() => {
+    fetchDiskData();
+  }, [fetchDiskData]);
+
+  let content: string | number | null = null;
+
+  if (!support) {
+    content = null;
+  }
+
+  if (storageEstimate.usage !== undefined && storageEstimate.quota) {
+    if (isNaN(storageEstimate.usage) || isNaN(storageEstimate.quota)) {
+      content = 'Unable to determine disk usage…';
+    } else {
+      const ratio = Math.round(
+        (storageEstimate.usage / storageEstimate.quota) * PERCENT,
+      );
+      content = `Using approximately ${getFileSize(
+        storageEstimate.usage,
+      )} out of ${getFileSize(storageEstimate.quota)} of available quota (${
+        ratio ? 'about' : 'less than'
+      } ${ratio}%)`;
+    }
+  }
+
+  return (
+    <section>
+      <h5>Disk usage</h5>
+      <p>{content != null && content}</p>
+    </section>
+  );
+};
+
+/*
 class DiskUsage extends PureComponent {
   constructor(props) {
     super(props);
@@ -41,6 +87,6 @@ class DiskUsage extends PureComponent {
       </section>
     );
   }
-}
+}*/
 
 export default DiskUsage;
