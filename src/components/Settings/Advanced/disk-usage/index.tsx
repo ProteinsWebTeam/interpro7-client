ed9d5a57f2af6d427d157d27a27f9357c3a13e1a
apolignano <alessandro.polignano@ebi.ac.uk>
2024-07-29 13:44:57 +0100

    Removed previous JS comments from DiskUsage component

--- src/components/Settings/Advanced/disk-usage/index.tsx
+++ src/components/Settings/Advanced/disk-usage/index.tsx
@@ -57,36 +57,4 @@ const DiskUsage = () => {
   );
 };
 
-/*
-class DiskUsage extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { usage: NaN, quota: NaN };
-  }
-
-  async componentDidMount() {
-    this.setState(await getEstimate());
-  }
-
-  render() {
-    if (!support) return null;
-    const { usage, quota } = this.state;
-    let content;
-    if (isNaN(usage) || isNaN(quota)) {
-      content = 'Unable to determine disk usage…';
-    } else {
-      const ratio = Math.round((usage / quota) * PERCENT);
-      content = `Using approximately ${getFileSize(usage)} out of ${getFileSize(
-        quota,
-      )} of available quota (${ratio ? 'about' : 'less than'} ${ratio}%)`;
-    }
-    return (
-      <section>
-        <h5>Disk usage</h5>
-        <p>{content}</p>
-      </section>
-    );
-  }
-}*/
-
 export default DiskUsage;
