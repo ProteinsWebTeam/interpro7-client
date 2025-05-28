b019a7621a00a2b715c3d3283a03485a1cf9a720
apolignano <alessandro.polignano@ebi.ac.uk>
2025-05-28 14:46:44 +0100

    Include short_name column in TSV download

--- src/web-workers/download/object2TSV.js
+++ src/web-workers/download/object2TSV.js
@@ -79,6 +79,7 @@ export const columns /*: {
   entry: [
     { name: 'Accession', selector: 'metadata.accession' },
     { name: 'Name', selector: 'metadata.name' },
+    { name: 'Short Name', selector: 'metadata.short_name' },
     { name: 'Source Database', selector: 'metadata.source_database' },
     { name: 'Type', selector: 'metadata.type' },
     { name: 'Integrated Into', selector: 'metadata.integrated' },
