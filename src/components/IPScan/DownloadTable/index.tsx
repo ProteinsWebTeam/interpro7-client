e14b4360fe3d630faf92f80d5214515518ed343a
apolignano <alessandro.polignano@ebi.ac.uk>
2025-07-14 11:20:48 +0100

    Page structure and version retrieval

--- src/components/IPScan/DownloadTable/index.tsx
+++ src/components/IPScan/DownloadTable/index.tsx
@@ -14,82 +14,95 @@ import tableCSS from 'components/Table/style.css';
 
 const css = cssBinder(tableCSS, fonts, local);
 
-interface LoadedProps extends LoadDataProps<{ tag_name: string }> {}
+type ReleasePayload = Array<{
+  tag_name: string;
+}>;
 
-export const DownloadTable = ({ data }: LoadedProps) => {
-  if (!data) return null;
-  const { loading, payload } = data;
-  if (loading || !payload) return <Loading />;
-  const { tag_name: version } = payload;
-  const [_, dataVersion] = version.split('-');
+interface LoadedProps
+  extends LoadDataProps<ReleasePayload, 'IPScan'>,
+    LoadDataProps<ReleasePayload, 'IPScan6'> {}
+
+export const DownloadTable = ({ dataIPScan, dataIPScan6 }: LoadedProps) => {
+  if (!dataIPScan || !dataIPScan6) return null;
+  if (dataIPScan?.loading || dataIPScan6?.loading) return <Loading />;
+
+  const version = dataIPScan.payload?.[0].tag_name;
+  const version6 = dataIPScan6.payload?.[0].tag_name;
+
+  //const [_, dataVersion] = version.split('-');
 
   return (
-    <table className={css('vf-table')}>
-      <thead>
-        <tr>
-          <th>Resource</th>
-          <th>Version</th>
-          <th>Download</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>InterProScan (64-bit Linux)</td>
-          <td>{version}</td>
-          <td>
-            <Link
-              href={`https://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/${version}/interproscan-${version}-64-bit.tar.gz`}
-            >
-              tar
-            </Link>
-            ,{' '}
-            <Link
-              href={`https://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/${version}/interproscan-${version}-64-bit.tar.gz.md5`}
-            >
-              md5
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <td>InterProScan</td>
-          <td>6.0.0-alpha</td>
-          <td>
-            <p>
-              To get started, install <strong>Nextflow</strong> (version
-              24.10.04 or later) along with a container runtime such as Docker,
-              SingularityCE, or Apptainer. Nextflow will automatically retrieve
-              the workflow from GitHub, and required data can be downloaded
-              during execution.
-            </p>
-            <p>
-              If you have Docker and Nextflow installed, run the following
-              command to test InterProScan and download required data:
-            </p>
-            <pre>
-              <code>
-                nextflow run ebi-pf-team/interproscan6 <br /> -r 6.0.0-alpha{' '}
-                <br /> -profile docker,test <br /> --datadir data <br />{' '}
-                --interpro latest <br /> --download
-              </code>
-            </pre>
-            <p>
-              For more information, refer to the official guide on{' '}
-              <a
-                href="https://github.com/ebi-pf-team/interproscan6"
-                target="_blank"
-                rel="noopener noreferrer"
+    <>
+      <h4> InterProScan 5 </h4>
+      <table className={css('vf-table')}>
+        <thead>
+          <tr>
+            <th>Resource</th>
+            <th>Version</th>
+            <th>Download</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Binaries and data</td>
+            <td>{version}</td>
+            <td>
+              <Link
+                href={`https://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/${version}/interproscan-${version}-64-bit.tar.gz`}
               >
-                GitHub
-              </a>
-              .
-            </p>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+                tar
+              </Link>
+              ,{' '}
+              <Link
+                href={`https://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/${version}/interproscan-${version}-64-bit.tar.gz.md5`}
+              >
+                md5
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h4> InterProScan 6 </h4>
+      <p> The latest version of InterProScan6 is {version6}. </p>
+      <p>
+        To get started, install Nextflow (version 24.10.04 or later) along with
+        a container runtime such as Docker, SingularityCE, or Apptainer.
+        Nextflow will automatically retrieve the workflow from GitHub, and
+        required data can be downloaded during execution.
+      </p>
+      <p>
+        If you have Docker and Nextflow installed, run the following command to
+        test InterProScan and download required data:
+      </p>
+      <pre>
+        <code>
+          nextflow run ebi-pf-team/interproscan6 <br /> -r 6.0.0-alpha <br />{' '}
+          -profile docker,test <br /> --datadir data <br /> --interpro latest{' '}
+          <br /> --download
+        </code>
+      </pre>
+      <p>
+        For more information, refer to the official guide on{' '}
+        <a
+          href="https://github.com/ebi-pf-team/interproscan6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </>
   );
 };
 
-export default loadData(getUrlForRelease('IPScan') as LoadDataParameters)(
-  DownloadTable,
+export default loadData<ReleasePayload, 'IPScan'>({
+  getUrl: getUrlForRelease('IPScan'),
+  propNamespace: 'IPScan',
+} as LoadDataParameters)(
+  loadData<ReleasePayload, 'IPScan6'>({
+    getUrl: getUrlForRelease('IPScan6'),
+    propNamespace: 'IPScan6',
+  } as LoadDataParameters)(DownloadTable),
 );
