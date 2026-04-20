9fdb32b34c8af43f9c7049861942df3d09900443
apolignano <alessandro.polignano@ebi.ac.uk>
2026-04-20 14:58:48 +0100

    Remove github repo reference, links and other dependencies

--- src/components/Settings/Advanced/index.tsx
+++ src/components/Settings/Advanced/index.tsx
@@ -26,42 +26,8 @@ const Advanced = () => {
     <section>
       <DiskUsage />
       <div>
-        {'This website has been built on '}
-        <code>{new Date(devInfo.buildTime).toString()}</code>
-      </div>
-      <div>
-        {'Build mode: '}
-        <code>{devInfo.mode}</code>
-      </div>
-      <div>
-        It has been built from the repository at:
-        <ul>
-          <li>
-            <Link target="_blank" href={url}>
-              <code>{url}</code>
-            </Link>
-          </li>
-          <li>
-            {'branch: '}
-            <Link target="_blank" href={`${url}/tree/${devInfo.git.branch}`}>
-              <code>{devInfo.git.branch}</code>
-            </Link>
-          </li>
-          {devInfo.git.tag !== devInfo.git.commit && devInfo.git.tag && (
-            <li>
-              {'tag: '}
-              <Link target="_blank" href={`${url}/tree/${devInfo.git.tag}`}>
-                <code>{devInfo.git.tag}</code>
-              </Link>
-            </li>
-          )}
-          <li>
-            {'commit: '}
-            <Link target="_blank" href={`${url}/tree/${devInfo.git.commit}`}>
-              <code>{devInfo.git.commit}</code>
-            </Link>
-          </li>
-        </ul>
+        <h5> Build commit </h5>
+        <code>{devInfo.git.commit}</code>
       </div>
     </section>
   );
